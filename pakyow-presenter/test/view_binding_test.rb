876fcb3dab746df4d1372f3359625003b56dd57f
jugyo <jugyo.org@gmail.com>
2011-09-20 12:52:33 +0900

    some tests for binding

---
+++ pakyow-presenter/test/view_binding_test.rb
@@ -0,0 +1,74 @@
+require 'helper'
+
+class Contact < Struct.new(:full_name, :email)
+end
+
+class ViewBindingTest < Test::Unit::TestCase
+  should "bind hash to view" do
+    view = create_view_from_string(<<-D)
+    <div class="contact">
+      <span itemprop="full_name">John Doe</span>
+      <a itemprop="email">john@example.com</a>
+    </div>
+    D
+    data = {:full_name => "Jugyo Kohno", :email => "jugyo@example.com"}
+    view.bind(data)
+
+    node = Nokogiri::HTML.fragment(view.to_html)
+    assert_equal data[:full_name], node.css('.contact span').first.content
+    assert_equal data[:email],     node.css('.contact a').first.content
+  end
+
+  should "bind an object to view" do
+    view = create_view_from_string(<<-D)
+    <div class="contact">
+      <span itemprop="contact[full_name]">John Doe</span>
+      <a itemprop="contact[email]">john@example.com</a>
+    </div>
+    D
+    data = Contact.new("Jugyo Kohno", "jugyo@example.com")
+    view.bind(data)
+
+    node = Nokogiri::HTML.fragment(view.to_html)
+    assert_equal data[:full_name], node.css('.contact span').first.content
+    assert_equal data[:email],     node.css('.contact a').first.content
+  end
+
+  should "bind an object to view with type" do
+    view = create_view_from_string(<<-D)
+    <div class="contact_info">
+      <span itemprop="contact_info[full_name]">John Doe</span>
+      <a itemprop="contact_info[email]">john@example.com</a>
+    </div>
+    D
+    data = Contact.new("Jugyo Kohno", "jugyo@example.com")
+    view.bind(data, "contact_info")
+
+    node = Nokogiri::HTML.fragment(view.to_html)
+    assert_equal data[:full_name], node.css('.contact_info span').first.content
+    assert_equal data[:email],     node.css('.contact_info a').first.content
+  end
+
+  should "bind an object to view by using 'name' attributes" do
+    view = create_view_from_string(<<-D)
+    <form class="contact">
+      <input type="text" name="contact[full_name]" value="John Doe" />
+      <input type="text" name="contact[email]" value="john@example.com" />
+      <input type="submit" value="Submit" />
+    </form>
+    D
+    data = Contact.new("Jugyo Kohno", "jugyo@example.com")
+    view.bind(data)
+
+    node = Nokogiri::HTML.fragment(view.to_html)
+    assert_equal data[:full_name], node.css("input[name='contact[full_name]']").attr("value").value
+    assert_equal data[:email],     node.css("input[name='contact[email]']").attr("value").value
+  end
+
+  private
+
+  def create_view_from_string(string)
+    doc = Nokogiri::HTML::Document.parse(string)
+    View.new(doc.root)
+  end
+end
