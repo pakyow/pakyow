5658649df3ce7e957afb4095213c0c00656db5b7
Bryan Powell <bryan@metabahn.com>
2011-10-10 17:28:40 -0500

    Rename Mailer#mail to message

--- pakyow-mailer/lib/mailer/mailer.rb
+++ pakyow-mailer/lib/mailer/mailer.rb
@@ -1,19 +1,19 @@
 module Pakyow
   class Mailer
-    attr_accessor :view, :mail
+    attr_accessor :view, :message
   
     def initialize(view_path)
       @view = Pakyow.app.presenter.view_for_full_view_path(view_path, true)
       
-      @mail               = Mail.new
-      @mail.from          = Configuration::Base.mailer.default_sender
-      @mail.content_type  = Configuration::Base.mailer.default_content_type
-      @mail.delivery_method(Configuration::Base.mailer.delivery_method, Configuration::Base.mailer.delivery_options)
+      @message               = Mail.new
+      @message.from          = Configuration::Base.mailer.default_sender
+      @message.content_type  = Configuration::Base.mailer.default_content_type
+      @message.delivery_method(Configuration::Base.mailer.delivery_method, Configuration::Base.mailer.delivery_options)
     end
     
     def deliver_to(recipient, subject = nil)
-      @mail.body    = self.view.to_html
-      @mail.subject = subject if subject
+      @message.body    = self.view.to_html
+      @message.subject = subject if subject
       
       if recipient.is_a?(Array)
         recipient.each {|r| deliver(r)}
@@ -23,10 +23,10 @@ module Pakyow
     end
     
     protected
-  
+    
     def deliver(recipient)
-      @mail.to = recipient
-      @mail.deliver
+      @message.to = recipient
+      @message.deliver
     end
   end
 end
