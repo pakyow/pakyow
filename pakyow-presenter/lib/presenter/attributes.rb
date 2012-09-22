717976419cadf4cd484a55a7af8d777cbb178083
Bryan Powell <bryan@metabahn.com>
2012-09-22 18:13:58 -0500

    Smarter handling of single, boolean, and multiple attribute values.

--- pakyow-presenter/lib/presenter/attributes.rb
+++ pakyow-presenter/lib/presenter/attributes.rb
@@ -1,26 +1,25 @@
 module Pakyow
   module Presenter
     class Attributes
+      @@types = {
+        :boolean  => [:selected, :checked, :disabled, :readonly, :multiple],
+        :multiple => [:class]
+      }
+
       def initialize(view)
         @view = view
       end
 
       def method_missing(method, *args)
+        attribute = method.to_s
+
         if method.to_s.include?('=')
-          attribute = method.to_s.gsub('=', '')
+          attribute = attribute.gsub('=', '')
           value = args[0]
 
-          if value.is_a? Proc
-            value = value.call(@view.doc[attribute])
-          end
-
-          if value.nil?
-            @view.doc.remove_attribute(attribute)
-          else
-            @view.doc[attribute] = value
-          end
+          self.set_attribute(attribute, value)
         else
-          return @view.doc[method.to_s]
+          self.get_attribute(attribute)
         end
       end
 
@@ -30,6 +29,47 @@ module Pakyow
       
       def id(*args)
         method_missing(:id, *args)
+      end
+
+      protected
+
+      def set_attribute(attribute, value)
+        type = self.type_of_attribute(attribute)
+
+        value = value.call(self.deconstruct_attribute_value_of_type(@view.doc[attribute], type)) if value.is_a? Proc
+        value = construct_attribute_value_of_type(value, type, attribute)
+        
+        if value.nil?
+          @view.doc.remove_attribute(attribute)
+        else
+          @view.doc[attribute] = value
+        end
+      end
+
+      def get_attribute(attribute)
+        self.deconstruct_attribute_value_of_type(@view.doc[attribute], self.type_of_attribute(attribute))
+      end
+
+      def type_of_attribute(attribute)
+        attribute = attribute.to_sym
+
+        return :boolean  if @@types[:boolean].include?(attribute)
+        return :multiple if @@types[:multiple].include?(attribute)
+        return :single
+      end
+
+      def deconstruct_attribute_value_of_type(value, type)
+        return value             if type == :single
+        return value.split(' ')  if type == :multiple
+        return !value.nil?       if type == :boolean
+      end
+
+      def construct_attribute_value_of_type(value, type, attribute)
+        return value if type == :single
+        return value.join(' ') if type == :multiple
+
+        # pp value
+        return value ? attribute : nil if type == :boolean
       end
     end
 
