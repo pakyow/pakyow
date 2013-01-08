837d338200c719ea48dfe4621a8cc889735a273e
Bryan Powell <bryan@metabahn.com>
2013-01-07 21:27:53 -0600

    Add default restful route template

---
+++ pakyow-core/lib/core/route_template_defaults.rb
@@ -0,0 +1,23 @@
+Pakyow::Router.instance.set(:default) {
+  template(:restful) { |data|
+    get '/', fn(:index)
+
+    # special case for show (view path is overridden)
+    if show_fns = fn(:show)
+      show_fns = [show_fns] unless show_fns.is_a?(Array)
+      get '/:id', show_fns.unshift(
+        lambda {
+          presenter.view_path = File.join(self.path, 'show') if Configuration::Base.app.presenter
+        }
+      )
+    end
+
+    get '/new', fn(:new)
+    post '/', fn(:create)
+
+    get '/:id/edit', fn(:edit)
+    put '/:id', fn(:update)
+
+    delete '/:id', fn(:delete)
+  }
+}
