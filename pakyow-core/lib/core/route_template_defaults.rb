25bdc6b3bad78b4e1420945a8619d11136d414b0
Bryan Powell <bryan@metabahn.com>
2013-04-01 07:42:45 -0500

    Bug fixes for restful template
    - Fix improper reference to `path`
    - Reorder so `show` doesn't match for `new` case

--- pakyow-core/lib/core/route_template_defaults.rb
+++ pakyow-core/lib/core/route_template_defaults.rb
@@ -9,17 +9,20 @@ module Pakyow
 
           default fn(:default) if fn(:default)
 
+          get '/new', :new, fn(:new) if fn(:new)
+          
           # special case for show (view path is overridden)
           if show_fns = fn(:show)
             show_fns = [show_fns] unless show_fns.is_a?(Array)
+            path = self.path
             get '/:id', :show, show_fns.unshift(
               lambda {
-                presenter.view_path = File.join(self.path, 'show') if Configuration::Base.app.presenter
+                presenter.view_path = File.join(path, 'show') if Configuration::Base.app.presenter
               }
             )
           end
 
-          get '/new', :new, fn(:new) if fn(:new)
+          
           post '/', :create, fn(:create) if fn(:create)
 
           get '/:id/edit', :edit, fn(:edit) if fn(:edit)
