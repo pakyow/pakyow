63d38446d7615dd6739d6c6a251c11e5ea0fc351
Ryan Clardy <ryan@metabahn.com>
2013-06-12 17:39:30 -0500

    Allow port to be specified on server startup
    pakyow server [environment] [-p port]

--- pakyow-core/lib/commands/server.rb
+++ pakyow-core/lib/commands/server.rb
@@ -4,5 +4,22 @@ else
   $:.unshift(Dir.pwd)
 
   require 'app'
-  Pakyow::App.run(ARGV.first)
+
+  valid_args = true
+  ARGV.each_with_index {|arg, index|
+  	if arg == "-p" || arg == "-port"
+  		if ARGV[index + 1] =~ /^\d{1,5}$/
+		  	Pakyow::Config::Server.port = ARGV[index + 1]
+		  	2.times {ARGV.delete_at(index)}
+		  else
+		  	valid_args = false
+		  end
+	  end
+  }
+
+  if valid_args
+	  Pakyow::App.run(ARGV.first)
+	else
+		puts File.open(File.join(CORE_PATH, 'commands/USAGE-SERVER')).read
+	end
 end
