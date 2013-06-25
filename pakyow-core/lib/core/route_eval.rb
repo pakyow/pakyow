8e822367bbdfe6c7be65a44bd9818a0f85031eb1
Ryan Clardy <ryan@metabahn.com>
2013-06-25 17:21:52 -0500

    Hooks for handlers

--- pakyow-core/lib/core/route_eval.rb
+++ pakyow-core/lib/core/route_eval.rb
@@ -83,10 +83,16 @@ module Pakyow
     # Creates a handler.
     #
     def handler(*args, &block)
-      name, code, fn = self.class.parse_handler_args(args)
-      fn = block if block_given?
+      name, code, fn, hooks = self.class.parse_handler_args(args)
 
-      @handlers << [name, code, [fn]]
+      fns = []
+      # add passed block to fns
+      fns << block if block_given?
+
+      # build the final list of fns
+      fns = self.class.build_fns(fns, hooks)
+
+      @handlers << [name, code, fns]
     end
 
     def group(*args, &block)
@@ -304,7 +310,9 @@ module Pakyow
       def parse_handler_args(args)
         ret = []
         args.each { |arg|
-          if arg.is_a?(Proc) # we have a fn
+          if arg.is_a?(Hash) # we have hooks
+            ret[3] = arg
+          elsif arg.is_a?(Proc) # we have a fn
             ret[2] = arg
           elsif arg.is_a?(Integer) # we have a code
             ret[1] = arg
