45a3fc6b87dec78e7a5c7f9c5c225df384eceb68
Bryan Powell <bryan@metabahn.com>
2013-06-25 22:20:20 -0500

    Better attribute support
    Can now do things like:
    
    attributes.class << 'foo'
    attributes.class.ensure('bar')
    attributes.class.any_array_method(…)

--- pakyow-presenter/lib/presenter/attributes.rb
+++ pakyow-presenter/lib/presenter/attributes.rb
@@ -1,13 +1,85 @@
 module Pakyow
   module Presenter
-    class Attributes
+    class Attribute
       @@types = {
         :boolean  => [:selected, :checked, :disabled, :readonly, :multiple],
         :multiple => [:class]
       }
 
+      def initialize(name, raw_value, control)
+        @type = type_of_attribute(name)
+        @name = name
+        @value = deconstruct_attribute_value_of_type(raw_value, @type)
+        @control = control
+      end
+
+      def set(value)
+        value = [value] if @type == :multiple && !value.is_a?(Array)
+        @value = value
+        update_value
+      end
+
+      # ensures that the value passed is contained in `value`
+      def ensure(value)
+        case @type
+        when :multiple
+          @value << value unless @value.include?(value)
+        when :boolean
+          @value = value
+        else
+          @value << value unless @value.match(value)
+        end
+
+        update_value
+      end
+
+      # passes method call to `value`
+      def method_missing(method, *args)
+        ret = @value.send(method, *args)
+        update_value
+
+        return ret
+      end
+
+      private
+
+      def update_value
+        updated_value = @value
+        updated_value = updated_value.call(deconstruct_attribute_value_of_type(@doc[@name], @type)) if updated_value.is_a? Proc
+        updated_value = construct_attribute_value_of_type(updated_value, @type, @name)
+
+        if updated_value.nil?
+          @control.remove_attribute(@name)
+        else
+          @control.update_value_for_attribute(@name, updated_value)
+        end
+      end
+
+      def type_of_attribute(attribute)
+        attribute = attribute.to_sym
+
+        return :boolean  if @@types[:boolean].include?(attribute)
+        return :multiple if @@types[:multiple].include?(attribute)
+        return :single
+      end
+
+      def deconstruct_attribute_value_of_type(value, type)
+        return value                          if type == :single
+        return value ? value.split(' ') : []  if type == :multiple
+        return !value.nil?                    if type == :boolean
+      end
+
+      def construct_attribute_value_of_type(value, type, attribute)
+        return value if type == :single
+        return value.join(' ') if type == :multiple
+        return value ? attribute : nil if type == :boolean
+      end
+    end
+
+    class Attributes
       def initialize(doc)
         @doc = doc
+        @attributes = {}
       end
 
       def method_missing(method, *args)
@@ -31,45 +103,26 @@ module Pakyow
         method_missing(:id, *args)
       end
 
+      def update_value_for_attribute(attribute, value)
+        @doc[attribute] = value
+      end
+
+      def remove_attribute(attribute)
+        @doc.remove_attribute(attribute)
+      end
+
       protected
 
       def set_attribute(attribute, value)
-        type = self.type_of_attribute(attribute)
-
-        value = value.call(self.deconstruct_attribute_value_of_type(@doc[attribute], type)) if value.is_a? Proc
-        value = construct_attribute_value_of_type(value, type, attribute)
-        
-        if value.nil?
-          @doc.remove_attribute(attribute)
-        else
-          @doc[attribute] = value
-        end
+        get_attribute(attribute).set(value)
       end
 
       def get_attribute(attribute)
-        self.deconstruct_attribute_value_of_type(@doc[attribute], self.type_of_attribute(attribute))
-      end
+        unless a = @attributes[attribute]
+          a = Attribute.new(attribute, @doc[attribute], self)
+        end
 
-      def type_of_attribute(attribute)
-        attribute = attribute.to_sym
-
-        return :boolean  if @@types[:boolean].include?(attribute)
-        return :multiple if @@types[:multiple].include?(attribute)
-        return :single
-      end
-
-      def deconstruct_attribute_value_of_type(value, type)
-        return value                          if type == :single
-        return value ? value.split(' ') : []  if type == :multiple
-        return !value.nil?                    if type == :boolean
-      end
-
-      def construct_attribute_value_of_type(value, type, attribute)
-        return value if type == :single
-        return value.join(' ') if type == :multiple
-
-        # pp value
-        return value ? attribute : nil if type == :boolean
+        return a
       end
     end
 
@@ -88,6 +141,14 @@ module Pakyow
         ret = []
         @attributes.each{|a| ret << a.send(method, *args)}
         ret
+      end
+
+      def class(*args)
+        method_missing(:class, *args)
+      end
+      
+      def id(*args)
+        method_missing(:id, *args)
       end
     end
   end
