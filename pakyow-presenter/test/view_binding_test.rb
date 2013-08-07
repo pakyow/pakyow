4defe50fe522f6bb4993a509451f09e51cd17aa0
Bryan Powell <bryan@metabahn.com>
2013-08-07 13:49:15 -0500

    Update view binding tests

--- pakyow-presenter/test/view_binding_test.rb
+++ pakyow-presenter/test/view_binding_test.rb
@@ -1,70 +1,61 @@
-# require 'support/helper'
+class Contact
+  attr_accessor :full_name, :email
 
-# class Contact < Struct.new(:full_name, :email)
-# end
+  def initialize(full_name, email)
+    @full_name = full_name
+    @email = email
+  end
 
-# class ViewBindingTest < Minitest::Test
-#   should "bind hash to view" do
-#     view = create_view_from_string(<<-D)
-#     <div class="contact">
-#       <span itemprop="full_name">John Doe</span>
-#       <a itemprop="email">john@example.com</a>
-#     </div>
-#     D
-#     data = {:full_name => "Jugyo Kohno", :email => "jugyo@example.com"}
-#     view.bind(data)
+  def [](key)
+    send(key)
+  end
+end
 
-#     assert_equal data[:full_name], view.doc.css('.contact span').first.content
-#     assert_equal data[:email],     view.doc.css('.contact a').first.content
-#   end
+class ViewBindingTest < Minitest::Test
+  def setup
+    @view = create_view_from_string(<<-D)
+    <div class="contact" data-scope="contact">
+      <span data-prop="full_name">John Doe</span>
+      <a data-prop="email">john@example.com</a>
+    </div>
+    D
+  end
 
-#   should "bind an object to view" do
-#     view = create_view_from_string(<<-D)
-#     <div class="contact">
-#       <span itemprop="contact[full_name]">John Doe</span>
-#       <a itemprop="contact[email]">john@example.com</a>
-#     </div>
-#     D
-#     data = Contact.new("Jugyo Kohno", "jugyo@example.com")
-#     view.bind(data)
+  def test_should_bind_hash
+    data = {:full_name => "Jugyo Kohno", :email => "jugyo@example.com"}
+    @view.scope(:contact).bind(data)
 
-#     assert_equal data[:full_name], view.doc.css('.contact span').first.content
-#     assert_equal data[:email],     view.doc.css('.contact a').first.content
-#   end
+    assert_equal data[:full_name], @view.doc.css('.contact span').first.content
+    assert_equal data[:email],     @view.doc.css('.contact a').first.content
+  end
 
-#   should "bind an object to view with type" do
-#     view = create_view_from_string(<<-D)
-#     <div class="contact_info">
-#       <span itemprop="contact_info[full_name]">John Doe</span>
-#       <a itemprop="contact_info[email]">john@example.com</a>
-#     </div>
-#     D
-#     data = Contact.new("Jugyo Kohno", "jugyo@example.com")
-#     view.bind(data, "contact_info")
+  def test_should_bind_object
+    data = Contact.new("Jugyo Kohno", "jugyo@example.com")
+    @view.scope(:contact).bind(data)
 
-#     assert_equal data[:full_name], view.doc.css('.contact_info span').first.content
-#     assert_equal data[:email],     view.doc.css('.contact_info a').first.content
-#   end
+    assert_equal data[:full_name], @view.doc.css('.contact span').first.content
+    assert_equal data[:email],     @view.doc.css('.contact a').first.content
+  end
 
-#   should "bind an object to view by using 'name' attributes" do
-#     view = create_view_from_string(<<-D)
-#     <form class="contact">
-#       <input type="text" name="contact[full_name]" value="John Doe" />
-#       <input type="text" name="contact[email]" value="john@example.com" />
-#       <input type="submit" value="Submit" />
-#     </form>
-#     D
-#     data = Contact.new("Jugyo Kohno", "jugyo@example.com")
-#     view.bind(data)
+  # def test_should_map_content_to_value
+  #   view = create_view_from_string(<<-D)
+  #   <div class="contact" data-scope="contact">
+  #     <input type="text" data-prop="full_name">
+  #   </div>
+  #   D
+  #
+  #   data = { full_name: 'foo' }
+  #   view.scope(:contact).bind(data)
+  #
+  #   assert_equal data[:full_name], view.doc.css('.contact input').first.get_attribute('value')
+  # end
 
-#     assert_equal data[:full_name], view.doc.css("input[name='contact[full_name]']").attr("value").value
-#     assert_equal data[:email],     view.doc.css("input[name='contact[email]']").attr("value").value
-#   end
+  #TODO test name on input is autoset
 
-#   private
+  private
 
-#   def create_view_from_string(string)
-#     doc = Nokogiri::HTML::Document.parse(string)
-#     View.new(doc.root)
-#   end
-# end
+  def create_view_from_string(string)
+    doc = Nokogiri::HTML::Document.parse(string)
+    View.new(doc.root)
+  end
+end
