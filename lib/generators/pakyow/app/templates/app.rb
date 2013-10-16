b83d08e6a547e2f5366a236b487ccd4548855de4
Bryan Powell <bryan@metabahn.com>
2013-10-16 15:22:26 -0500

    Add prototype env to generated app

--- lib/generators/pakyow/app/templates/app.rb
+++ lib/generators/pakyow/app/templates/app.rb
@@ -5,6 +5,11 @@ Pakyow::App.define do
     # All development-specific configuration goes here.
   end
 
+  configure :prototype do
+    # An environment for running just the front-end prototype.
+    app.ignore_routes = true
+  end
+
   configure :production do
     # Alternate environments can be configured, like this one.
   end
