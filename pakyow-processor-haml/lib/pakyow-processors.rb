c8a9b102e90b31e295ee8be4c42c681a0e8c1ef1
Bryan Powell <bryan@metabahn.com>
2013-10-26 09:54:01 -0500

    Remove MD/HAML processors
    These are separate gems, not to be bundled with pakyow

--- pakyow-processor-haml/lib/pakyow-processors.rb
+++
@@ -1,5 +0,0 @@
-require 'haml'
-
-Pakyow::App.processor :haml do |content|
-  Haml::Engine.new(content).render
-end
--- pakyow-processor-markdown/lib/pakyow-processor-markdown.rb
+++
@@ -1,5 +0,0 @@
-require 'rdiscount'
-
-Pakyow::App.processor :md, :mdown, :markdown do |content|
-  RDiscount.new(content).to_html
-end
