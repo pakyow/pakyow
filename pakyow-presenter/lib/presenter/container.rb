eeb5c63f75f3bf5a1d9c59587f01d55f166e4bff
Bryan Powell <bryan@metabahn.com>
2014-02-10 20:51:14 -0600

    Remove unnecessary code

--- pakyow-presenter/lib/presenter/container.rb
+++ pakyow-presenter/lib/presenter/container.rb
@@ -1,12 +1,6 @@
 module Pakyow
   module Presenter
     class Container < View
-      attr_accessor :composer
-
-      def invalidate!
-        @composer.dirty! unless @composer.nil?
-        super
-      end
     end
   end
 end
