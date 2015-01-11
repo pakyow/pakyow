fa0f6d478c73c01474fdf3137aeea146760e87d8
Bryan Powell <bryan@metabahn.com>
2015-01-11 14:17:17 -0600

    Require pakyow-support in presenter tests

--- pakyow-presenter/test/support/helper.rb
+++ pakyow-presenter/test/support/helper.rb
@@ -6,6 +6,7 @@ require 'minitest/autorun'
 require 'pry'
 require 'pp'
 
+require File.expand_path('../../../../pakyow-support/lib/pakyow-support', __FILE__)
 require File.expand_path('../../../../pakyow-core/lib/pakyow-core', __FILE__)
 require File.expand_path('../../../lib/pakyow-presenter', __FILE__)
 require File.expand_path('../../../../pakyow-core/test/support/helper', __FILE__)
