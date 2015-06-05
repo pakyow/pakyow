d38c05b372e8e2b44c4973dd5307db253db731fd
Bryan Powell <bryan@metabahn.com>
2015-06-05 16:38:19 -0500

    Remove old mailer tests

--- pakyow-mailer/spec/support/test_application.rb
+++ pakyow-mailer/spec/support/test_application.rb
@@ -1,6 +1,6 @@
 Pakyow::App.define do
   configure(:test) do
-    presenter.view_stores[:default] = "test/views"
+    presenter.view_stores[:default] = "spec/views"
   end
 
   # This keeps the app from actually being run.
