2b8fd6e586f9ffdcd87045ee01d112a068449ea4
jphager2 <jphager2@gmail.com>
2015-06-15 10:40:56 +0200

    issue 79 base

---
+++ pakyow-presenter/lib/presenter/null_view.rb
@@ -0,0 +1,16 @@
+module Pakyow
+  module UI
+    class NullView
+      def initialize(message)
+        @message = message
+      end
+
+      def method_missing(method, *args, &block)
+        raise NoViewError.new(@message)
+      end
+    end
+
+    class NoViewError < Exception
+    end
+  end
+end
