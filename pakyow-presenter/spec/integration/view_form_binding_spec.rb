ad83da7d4574c166d4b6a4d2e51152048d70bdfb
Bryan Powell <bryan@metabahn.com>
2015-10-17 15:38:32 -0500

    Remove nokogiridoc

--- pakyow-presenter/spec/integration/view_form_binding_spec.rb
+++ pakyow-presenter/spec/integration/view_form_binding_spec.rb
@@ -1,14 +1,6 @@
 require_relative 'support/int_helper'
 
 describe Pakyow::Presenter::View do
-  describe 'with NokogiriDoc' do
-    before do
-      Pakyow::Config.presenter.view_doc_class = Pakyow::Presenter::NokogiriDoc
-    end
-
-    include_examples :form_binding_specs
-  end
-
   describe 'with StringDoc' do
     before do
       Pakyow::Config.presenter.view_doc_class = Pakyow::Presenter::StringDoc
