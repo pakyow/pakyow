7cee26a530ac5c9df067546e82768163a2c2dad2
jphager2 <jphager2@gmail.com>
2015-12-14 16:56:53 +0100

    Remove app staging from test

--- pakyow-presenter/spec/unit/app/resource_bindings_spec.rb
+++ pakyow-presenter/spec/unit/app/resource_bindings_spec.rb
@@ -1,22 +1,19 @@
-require_relative '../../integration/support/int_helper'
-#require 'core/app'
+require 'spec_helper'
+require_relative '../../../../pakyow-core/spec/support/helpers/req_res_helpers.rb'
 
 describe Pakyow::App, '#resource' do
   include ReqResHelpers
 
-  before do
-    Pakyow::App.stage(:test)
-    Pakyow.app.context = Pakyow::AppContext.new(mock_request, mock_response)
-  end
+  let(:app) { Pakyow::App.new }
 
   context 'called with a resource name, path, and block' do
     before do
-      Pakyow.app.resource :test, "tests" do
+      app.resource :test, "tests" do
         list do
         end
       end
 
-      Pakyow.app.presenter.load
+      app.presenter.load
     end
 
     it 'creates a binding set for the resource name' do
@@ -25,7 +22,7 @@ describe Pakyow::App, '#resource' do
     end
 
     describe 'the binding set block' do
-      let(:binding_set_block) { Pakyow.app.bindings[:main] }
+      let(:binding_set_block) { app.bindings[:main] }
 
       it 'exists' do
         expect(binding_set_block).to be_kind_of Proc 
@@ -52,14 +49,14 @@ describe Pakyow::App, '#resource' do
     end
 
     it 'presenter does not override core method' do
-      expect(Pakyow.app.resource(:test)).to eq Pakyow::App.routes[:test]
+      expect(app.resource(:test)).to eq Pakyow::App.routes[:test]
     end
   end
 
   context 'called without a path' do
     it 'presenter does not override core method' do
-      no_path_passed = Proc.new { Pakyow.app.resource(:test) {} }
-      nil_path_passed = Proc.new { Pakyow.app.resource(:test, nil) {} }
+      no_path_passed = Proc.new { app.resource(:test) {} }
+      nil_path_passed = Proc.new { app.resource(:test, nil) {} }
       expect(no_path_passed).to raise_error ArgumentError
       expect(nil_path_passed).to raise_error ArgumentError
     end
@@ -67,8 +64,8 @@ describe Pakyow::App, '#resource' do
 
   context 'called without a resource name' do
     it 'presenter does not override core method' do
-      no_name_passed = Proc.new { Pakyow.app.resource() {} }
-      nil_name_passed = Proc.new { Pakyow.app.resource(nil, "tests") {} }
+      no_name_passed = Proc.new { app.resource() {} }
+      nil_name_passed = Proc.new { app.resource(nil, "tests") {} }
       expect(no_name_passed).to raise_error ArgumentError
       expect(nil_name_passed).to raise_error ArgumentError
     end
