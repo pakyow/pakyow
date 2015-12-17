5ab3142827fcbe66dfc39a76c9f2e08beebefde8
Bryan Powell <bryan@metabahn.com>
2015-12-17 15:55:12 -0600

    Few changes to Running helpers, with specs!

--- pakyow-core/lib/core/helpers/running.rb
+++ pakyow-core/lib/core/helpers/running.rb
@@ -1,15 +1,22 @@
+require 'rack/builder'
+require 'rack/handler'
+
 module Pakyow
   module Helpers
     # Methods run running an app.
     #
     # @api public
     module Running
+      STOP_METHODS = ['stop!', 'stop']
+      HANDLERS = ['puma', 'thin', 'mongrel', 'webrick']
+      SIGNALS = [:INT, :TERM]
+
       # Prepares the app for being staged in one or more environments by
       # loading config(s), middleware, and setting the load path.
       #
       # @api public
       def prepare(*env_or_envs)
-        return if prepared?
+        return true if prepared?
 
         # load config for one or more environments
         load_config(*env_or_envs)
@@ -33,29 +40,30 @@ module Pakyow
       #
       # @api public
       def run(*env_or_envs)
-        return if running?
-
-        @running = true
+        return true if running?
 
         builder.run(stage(*env_or_envs))
         detect_handler.run(builder, Host: config.server.host, Port: config.server.port) do |server|
-          trap(:INT)  { stop(server) }
-          trap(:TERM) { stop(server) }
+          SIGNALS.each do |signal|
+            trap(signal) { stop(server) }
+          end
         end
+
+        @running = true
       end
 
       # Returns true if the application is prepared.
       #
       # @api public
       def prepared?
-        @prepared
+        @prepared == true
       end
 
       # Returns true if the application is running.
       #
       # @api public
       def running?
-        @running
+        @running == true
       end
 
       # Returns true if the application is staged.
@@ -65,34 +73,44 @@ module Pakyow
         !Pakyow.app.nil?
       end
 
-      protected
-
+      # Returns a rack builder instance.
+      #
+      # @api public
       def builder
         @builder ||= Rack::Builder.new
       end
 
+      # Returns an instance of the rack handler.
+      #
+      # @api private
       def detect_handler
-        handlers = ['puma', 'thin', 'mongrel', 'webrick']
-        handlers.unshift(config.server.handler) if config.server.handler
+        if config.server.handler
+          HANDLERS.unshift(config.server.handler).uniq!
+        end
 
-        handlers.each do |handler|
+        HANDLERS.each do |handler_name|
           begin
-            return Rack::Handler.get(handler)
+            handler = Rack::Handler.get(handler_name)
+            return handler unless handler.nil?
           rescue LoadError
           rescue NameError
           end
         end
+
+        raise 'No handler found'
       end
 
+      protected
+
       def stop(server)
-        if server.respond_to?('stop!')
-          server.stop!
-        elsif server.respond_to?('stop')
-          server.stop
-        else
-          # exit ungracefully if necessary...
-          Process.exit!
+        STOP_METHODS.each do |method|
+          if server.respond_to?(method)
+            return server.send(method)
+          end
         end
+
+        # exit ungracefully
+        Process.exit!
       end
 
       def load_middleware
