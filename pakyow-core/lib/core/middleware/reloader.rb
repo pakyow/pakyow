51795c2243b0d1ad605f967e948d3add4663a4a6
Bryan Powell <bryan@metabahn.com>
2015-12-20 18:36:20 -0600

    Tests and inline docs for reloader middleware

--- pakyow-core/lib/core/middleware/reloader.rb
+++ pakyow-core/lib/core/middleware/reloader.rb
@@ -1,10 +1,13 @@
 module Pakyow
   module Middleware
+    # Rack compatible middleware that tells app to reload on each request.
+    #
+    # @api public
     class Reloader
       def initialize(app)
         @app = app
       end
-      
+
       def call(env)
         Pakyow.app.reload
         @app.call(env)
