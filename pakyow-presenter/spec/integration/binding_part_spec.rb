61840dfbbf4f8df9062d947e5a0d7b448db9b509
jphager2 <jphager2@gmail.com>
2015-12-29 13:28:47 +0100

    Add data-parts tests

--- pakyow-presenter/spec/integration/binding_part_spec.rb
+++ pakyow-presenter/spec/integration/binding_part_spec.rb
@@ -67,14 +67,58 @@ describe 'defining a binding part' do
         end
       end
       presenter.load
+      view.bind({})
+    end
+
+    let(:view) do
+      Pakyow::Presenter::View.new("<div data-scope=\"foo\"><a data-prop=\"bar\" class=\"lettus tomato burger\">Test Link</a></div>")
+    end
+
+    let(:class_value) do
+      view.scope(:foo).prop(:bar).attrs.first.instance_eval("get_attribute(:class)").value
+    end
+
+    let(:content_value) do
+      view.scope(:foo).prop(:bar).html.join
+    end
+
+    let(:href_value) do
+      view.scope(:foo).prop(:bar).attrs.first.instance_eval("get_attribute(:href)").value
     end
 
     it "defines multiple parts" do
-      # TODO test class is applied
       expect(data).to have_key :href 
       expect(data).to have_key :content 
       expect(data[:href].call()).to eq "/path/to/foo/bar"
       expect(data[:content].call()).to eq "Link to foo bar"
+
+      # TODO test class is applied in a better way
+      expect(class_value - %w{ tomato burger bacon }).to be_empty
+      expect(class_value).to include "bacon"
+    end
+
+    describe "using data-parts" do
+      let(:view) do
+        Pakyow::Presenter::View.new("<div data-scope=\"foo\"><a data-prop=\"bar\" data-parts=\"class\" class=\"lettus tomato burger\">Test Link</a></div>")
+      end
+
+      it "binds some parts to the view" do
+        expect(class_value).to eq %w{ tomato burger bacon }
+        expect(href_value).to be_empty
+        expect(content_value).to eq "Test Link"
+      end
+    end
+
+    describe "using data-parts-exclude" do
+      let(:view) do
+        Pakyow::Presenter::View.new("<div data-scope=\"foo\"><a data-prop=\"bar\" data-parts-exclude=\"class\" class=\"lettus tomato burger\">Test Link</a></div>")
+      end
+
+      it "doesnt bind other parts to the view" do
+        expect(class_value).to eq %w{ lettus tomato burger }
+        expect(href_value).to eq "/path/to/foo/bar"
+        expect(content_value).to eq "Link to foo bar"
+      end
     end
   end
 
