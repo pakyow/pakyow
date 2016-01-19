6104006807c9456dca36526fc057dabb168b2873
Jay Hayes <ur@iamvery.com>
2016-01-19 13:01:52 -0600

    Remove unused files

--- lib/commands/console.rb
+++
@@ -1,9 +0,0 @@
-require "pakyow/commands/console"
-
-if ARGV.first == '--help' || ARGV.first == '-h'
-  puts File.open(File.join(PAK_PATH, 'commands/USAGE-CONSOLE')).read
-else
-  Pakyow::Commands::Console
-    .new(environment: ARGV.first)
-    .run
-end
