dc1bab2ae0017f4a84e6c4dcc6203f67a98c5462
Jay Hayes <ur@iamvery.com>
2016-01-19 18:51:47 -0600

    Refactor spec to work with Ruby 2.0
    * Required kwargs weren't introduced until 2.1
    * This doesn't read quite as well IMO, but it's test code so nbd.

--- spec/features/command_line_interface_spec.rb
+++ spec/features/command_line_interface_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-RSpec.shared_examples "help information" do |command:|
+RSpec.shared_examples "help information" do |command|
   it "prints help information" do
     output = `#{command}`
 
@@ -10,12 +10,12 @@ end
 
 RSpec.describe "command line interface" do
   describe "help" do
-    it_behaves_like "help information", command: "bin/pakyow --help"
-    it_behaves_like "help information", command: "bin/pakyow -h"
+    it_behaves_like "help information", "bin/pakyow --help"
+    it_behaves_like "help information", "bin/pakyow -h"
 
     %w(console new server).each do |command|
       describe "#{command} help" do
-        it_behaves_like "help information", command: "bin/pakyow help #{command}"
+        it_behaves_like "help information", "bin/pakyow help #{command}"
       end
     end
   end
