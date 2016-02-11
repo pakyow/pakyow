fcae47ce8cc81a12fd89d2309100abc6dc473611
Karim Tarek <karimmtarek@gmail.com>
2016-02-10 19:22:01 -0500

    Remove trailing  in request path

--- pakyow-core/lib/core/middleware/req_path_normalizer.rb
+++ pakyow-core/lib/core/middleware/req_path_normalizer.rb
@@ -6,8 +6,9 @@ module Pakyow
       builder.use Pakyow::Middleware::ReqPathNormalizer
     end
 
-    # Rack compatible middleware that normalize the path if contains '//',
-    # it replace '//' with '/' and issue a 301 redirect to the new path.
+    # Rack compatible middleware that normalize the path if contains '//'
+    # or has a trailing '/', it replace '//' with '/', remove trailing `/`
+    # and issue a 301 redirect to the normalized path.
     #
     # @api public
     class ReqPathNormalizer
@@ -16,15 +17,27 @@ module Pakyow
       end
 
       def call(env)
-        if env['PATH_INFO'].include? '//'
-          CallContext.new(env).redirect(normalize_path(env['PATH_INFO']), 301)
+        path = env['PATH_INFO']
+
+        if double_slash?(path) || tail_slash?(path)
+          CallContext.new(env).redirect(normalize_path(path), 301)
         else
           @app.call(env)
         end
       end
 
       def normalize_path(path)
-        path.gsub('//', '/')
+        path
+          .gsub('//', '/')
+          .gsub(/(\/)+$/, '')
+      end
+
+      def double_slash?(path)
+        path.include?('//')
+      end
+
+      def tail_slash?(path)
+        (/(\/)+$/ =~ path).nil? ? false : true
       end
     end
   end
