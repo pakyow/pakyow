76b41989c371f47bc356bc173c21fdbf1df013db
Bryan Powell <bryan@metabahn.com>
2016-03-04 11:20:55 -0600

    Use ViewContext in mailer
    This makes it possible to use mutators to render views in mailer.

--- pakyow-mailer/lib/mailer/helpers.rb
+++ pakyow-mailer/lib/mailer/helpers.rb
@@ -1,7 +1,7 @@
 module Pakyow
   module Helpers
     def mailer(view_path)
-      Mailer.from_store(view_path, @presenter.store)
+      Mailer.from_store(view_path, @presenter.store, self)
     end
   end
 end
