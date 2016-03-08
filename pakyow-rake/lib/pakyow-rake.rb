73f4d4c18dfbdc9db27aa6a0aa38d0cd5f3b38f7
Jay Hayes <ur@iamvery.com>
2016-03-08 17:58:11 -0600

    Move rake gem into pakyow namespace

--- pakyow-rake/lib/pakyow-rake.rb
+++ pakyow-rake/lib/pakyow-rake.rb
@@ -1,4 +1,4 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-Dir["#{libdir}/tasks/*.rake"].sort.each { |task| load task }
+Dir["#{libdir}/pakyow/tasks/*.rake"].sort.each { |task| load task }
