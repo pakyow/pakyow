044994a6533bb5616a11c17603d30ecb066f9212
Jay Hayes <ur@iamvery.com>
2016-03-08 18:23:38 -0600

    Move Aargv constant to pakyow namespace

--- pakyow-support/lib/pakyow/support/aargv.rb
+++ pakyow-support/lib/pakyow/support/aargv.rb
@@ -1,13 +1,15 @@
-class Aargv
-  def self.normalize(args, opts)
-    Hash[opts.map { |opt_name, opt_opts|
-      type, default = opt_opts
+module Pakyow
+  class Aargv
+    def self.normalize(args, opts)
+      Hash[opts.map { |opt_name, opt_opts|
+        type, default = opt_opts
 
-      [opt_name, value_of_type(args, type) || default]
-    }.reject { |pair| pair[1].nil? }]
-  end
+        [opt_name, value_of_type(args, type) || default]
+      }.reject { |pair| pair[1].nil? }]
+    end
 
-  def self.value_of_type(values, type)
-    values.find { |value| value.is_a?(type) }
+    def self.value_of_type(values, type)
+      values.find { |value| value.is_a?(type) }
+    end
   end
 end
