e7f03409b0d3a71e8ad34744e5c8039e49b22253
Bryan Powell <bryan@metabahn.com>
2016-06-20 15:14:23 -0500

    Start the server on localhost by default

--- pakyow-core/lib/pakyow/core/config/server.rb
+++ pakyow-core/lib/pakyow/core/config/server.rb
@@ -3,7 +3,7 @@ Pakyow::Config.register :server do |config|
   config.opt :port, 3000
 
   # the host to start `pakyow server`
-  config.opt :host, '0.0.0.0'
+  config.opt :host, 'localhost'
 
   # explicitly set a handler to try (e.g. puma)
   config.opt :handler
