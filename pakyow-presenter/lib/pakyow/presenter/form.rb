a3b8192c6885f02d79d01a8005320a09d0681aea
jphager2 <jphager2@gmail.com>
2016-09-12 16:44:39 +0200

    refactor Pakyow::Presenter::Form

--- pakyow-presenter/lib/pakyow/presenter/form.rb
+++ pakyow-presenter/lib/pakyow/presenter/form.rb
@@ -1,22 +1,25 @@
 module Pakyow
   module Presenter
     class Form
+      attr_reader :view, :context
 
-      attr_reader :form_view, :context
+      PATCH_METHOD_INPUT = <<-HTML
+        <input type="hidden" name="_method" value="patch">
+      HTML
 
       def initialize(view, context)
-        @form_view = view
+        @view = view
         @context = context
       end
 
       def create(*binding_args, &block)
-        bind_data(binding_args, block) do |routes, view|
+        bind_form_action(binding_args, block) do |view|
           view.attrs.action = routes.path(:create, request_params) if routes
         end
       end
 
       def update(*binding_args, &block)
-        bind_data(binding_args, block) do |routes, view|
+        bind_form_action(binding_args, block) do |view|
           if routes
             route_params = request_params.merge(
               :"#{scoped_as}_id" => view.attrs.__send__('data-id').value
@@ -25,19 +28,15 @@ module Pakyow
             view.attrs.action = routes.path(:update, route_params)
           end
 
-          view.prepend(
-            View.new('<input type="hidden" name="_method" value="patch">')
-          )
+          view.prepend(View.new(PATCH_METHOD_INPUT))
         end
       end
 
       private
 
-      def bind_data(binding_args, block)
+      def bind_form_action(binding_args, block)
         bind(*binding_args) do |view, data|
-          routes = Router.instance.group(scoped_as)
-
-          yield(routes, view)
+          yield(view)
 
           return if block.nil?
 
@@ -49,13 +48,17 @@ module Pakyow
         end
       end
 
+      def routes
+        @routes ||= Router.instance.group(scoped_as)
+      end
+
       def request_params
         context.request.params
       end
 
       def method_missing(method, *args, &block)
-        if form_view.respond_to?(method)
-          form_view.__send__(method, *args, &block)
+        if view.respond_to?(method)
+          view.__send__(method, *args, &block)
         else
           super
         end
