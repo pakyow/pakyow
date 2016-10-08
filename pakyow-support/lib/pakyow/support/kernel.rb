b6c7aa927dfde906c1e960503faa2ffbd2d80b8b
Bryan Powell <bryan@metabahn.com>
2016-10-08 16:59:49 -0700

    Replace kernel extension with silenceable

--- pakyow-support/lib/pakyow/support/kernel.rb
+++
@@ -1,9 +0,0 @@
-module Kernel
-  def self.silence_warnings(&block)
-    warn_level = $VERBOSE
-    $VERBOSE = nil
-    result = block.call
-    $VERBOSE = warn_level
-    result
-  end
-end
---
+++ pakyow-support/lib/pakyow/support/silenceable.rb
@@ -0,0 +1,19 @@
+module Pakyow
+  module Support
+    # Methods for silencing various output.
+    #
+    module Silenceable
+      def self.silence_warnings
+        original_verbosity = $VERBOSE
+        $VERBOSE = nil
+        yield
+      ensure
+        $VERBOSE = original_verbosity
+      end
+
+      def silence_warnings(&block)
+        Silenceable.silence_warnings(&block)
+      end
+    end
+  end
+end
