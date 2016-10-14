3b0eb1ac5d1d77bf9d4310917b122505e5cf7d80
Bryan Powell <bryan@metabahn.com>
2016-10-13 17:18:35 -0700

    App defineable middleware

--- lib/pakyow.rb
+++ lib/pakyow.rb
@@ -1,2 +1,3 @@
 require "pakyow/version"
 require "pakyow/environment"
+require "pakyow/hooks"
--- pakyow-core/lib/pakyow/core/hooks.rb
+++ pakyow-core/lib/pakyow/core/hooks.rb
@@ -1,24 +1,16 @@
 module Pakyow
-  before :setup do
-    config.middleware.default.each do |middleware|
-      use middleware
-    end
-
-    # TODO: make this opt-in
-    # if Config.app.enforce_www
-    #   use Middleware::WWWEnforcer
-    # else
-    #   use Middleware::NonWWWEnforcer
-    # end
-
-    # TODO: I'd like a normalizer enabled option
-    # that controls this and www / non; maybe
-    # even combine them into a single normalizer
-    use Middleware::ReqPathNormalizer
-    use Middleware::Logger
-  end
-
   CallContext.after :error do
     logger.houston(req.error)
   end
+
+  App.after :configure do
+    if config.session.enabled
+      builder.use config.session.object, config.session.options
+    end
+
+    if config.app.static
+      builder.use Middleware::Static
+    end
+  end
 end
+
