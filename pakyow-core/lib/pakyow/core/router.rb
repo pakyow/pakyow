e06d0a5c8707e2eb1e8aad20d2bfca98a340288b
Bryan Powell <bryan@metabahn.com>
2016-12-04 18:44:42 -0800

    Initial routing refactor
    It mostly works, but there are quite a few things to cleanup. Also needs
    tests,  inline api documentation, and user-facing documentation.

--- pakyow-core/lib/pakyow/core/router.rb
+++ pakyow-core/lib/pakyow/core/router.rb
@@ -1,174 +1,397 @@
-require 'singleton'
+require "pakyow/support/aargv"
+require "pakyow/support/array"
+
+# TODO: group / namespace in templates
+
+# TODO: formats
+# TODO: within
 
 module Pakyow
-  # A singleton that manages route sets.
-  #
+  # TODO:
+  #   - the route being defined in context with logic
+  #   - how to use routers (direct vs through an app)
+  #   - how routes are defined at particular paths
+  #     - and what types are supported
+  #     - note that HEAD is not handled here
+  #   - named routes and how they work with lookups
+  #   - defined functions and how the can be used
+  #   - using named functions as hooks
+  #   - mention namespaces, groups, templates
+  #     - point them to other docs inline
+  #   - routing mixins and how they can be used
   class Router
-    include Singleton
+    METHOD_GET    = "GET".freeze
+    METHOD_POST   = "POST".freeze
+    METHOD_PUT    = "PUT".freeze
+    METHOD_PATCH  = "PATCH".freeze
+    METHOD_DELETE = "DELETE".freeze
 
-    attr_reader :sets
+    SUPPORTED_METHODS = [
+      METHOD_GET,
+      METHOD_POST,
+      METHOD_PUT,
+      METHOD_PATCH,
+      METHOD_DELETE
+    ].freeze
 
-    def initialize
-      @sets = {}
-    end
-
-    #TODO want to do this for all sets?
-    def reset
-      @sets = {}
-      self
-    end
-
-    # Iterates through route sets and returns the first matching route.
+    # Routing extensions that are automatically available in each router.
     #
-    def route(name, group = nil)
-      @sets.each { |set|
-        if r = set[1].route(name, group)
-          return r
-        end
+    DEFAULT_EXTENSIONS = [
+      "Pakyow::Routing::Extensions::Restful"
+    ].freeze
+
+    # TODO: rethink this a bit once we can define groups / namespaces in a template
+    # this feels kind of wrong, in that it's used as the path when building
+    attr_accessor :nested_path
+
+    # @api private
+    attr_reader :name, :path, :funcs, :hooks, :routes, :children, :templates
+
+    # TODO: describe all the ways it can be initialized
+    def initialize(name_or_path = nil, path_or_name = nil, before: [], after: [], around: [], &block)
+      # TODO: support regex path
+      args  = Aargv.normalize([name_or_path, path_or_name], name: Symbol, path: String)
+      name, path = args.values_at(:name, :path)
+
+      @name  = name
+      @path  = path
+
+      @hooks = {
+        before: Array.ensure(before),
+        after: Array.ensure(after),
+        around: Array.ensure(around)
       }
 
-      raise MissingRoute, "Could not find route '#{name}'"
+      @funcs = {}
+      @children = []
+      @templates = {}
+
+      @routes = SUPPORTED_METHODS.each_with_object({}) do |method, routes_hash|
+        routes_hash[method] = []
+      end
+
+      DEFAULT_EXTENSIONS.each do |extension|
+        extend Object.const_get(extension)
+      end
+
+      instance_eval(&block) if block_given?
     end
 
-    def exists?(context)
-      !match(context.request).empty?
-    end
-
-    # Performs the initial routing for a request.
+    # Creates a named function that can later be used as an endpoint
+    # for a route or as a route hook.
     #
-    def perform(context, app = Pakyow.app, &after_match)
-      fns = match(context.request)
-      after_match.call if block_given?
-
-      trampoline(fns, app)
-    end
-
-    # Reroutes a request.
-    #
-    def reroute(request)
-      throw :fns, self.match(request)
-    end
-
-    # Finds and invokes a handler by name or by status code.
-    #
-    def handle(name_or_code, app = Pakyow.app, from_logic = false)
-      app.response.status = name_or_code if name_or_code.is_a?(Integer)
-
-      @sets.each { |set|
-        if h = set[1].handle(name_or_code)
-          app.response.status = h[1]
-          from_logic ? throw(:fns, h[2]) : self.trampoline(h[2], app)
-          break
-        end
-      }
-    end
-
-    # Looks up and populates a path with data
-    #
-    def path(name_or_path, data = nil)
-      if name_or_path.is_a?(String)
-        return name_or_path
-      elsif name_or_path.is_a?(Hash)
-        RouteLookup.new.group(name_or_path.keys[0]).path(name_or_path.values[0], data)
+    def func(name, &block)
+      if block_given?
+        @funcs[name] = block
       else
-        RouteLookup.new.path(name_or_path, data)
+        @funcs.fetch(name) {
+          raise NameError, "Undefined func `#{name}` for Router"
+        }
       end
     end
 
-    # Looks up a route grouping
+    # Creates a default route (synonmous with `get "/"`).
     #
-    def group(name)
-      RouteLookup.new.group(name)
+    def default(func: nil, **hooks, &block)
+      get :default, "/", func: func, **hooks, &block
     end
 
-    # Calls a defined fn
-    #
-    def fn(name, app = nil)
-      @sets.each { |set|
-        if fn = set[1].fn(name)
-          if app.nil?
-            fn.call
-          else
-            app.instance_exec(&fn)
-          end
+    SUPPORTED_METHODS.each do |method|
+      nice_method = method.downcase.to_sym
+      define_method nice_method do |name_or_path = nil, path_or_name = nil, func: nil, **hooks, &block|
+        build_route(method, name_or_path, path_or_name, func: func, **hooks, &block)
+      end
+    end
 
-          break
+    # Creates a nested group of routes, with an optional name and
+    # hooks. Hooks defined on the group will be inherited by each
+    # route present in the group. Groups also inherit hooks
+    # defined in their parent scopes.
+    #
+    # Named groups make the routes available for path building.
+    # Paths to routes defined in unnamed groups are referenced
+    # by the most direct parent group that is named.
+    #
+    # @example Defining a group:
+    #   Pakyow::Router.new do
+    #     func :foo do
+    #       logger.info "foo"
+    #     end
+    #
+    #     group :foo, before: [:foo] do
+    #       func :bar do
+    #         logger.info "bar"
+    #       end
+    #
+    #       get :bar, "/bar", before: [:bar] do
+    #         # "foo" and "bar" have both been logged
+    #         send "foo.bar"
+    #       end
+    #     end
+    #
+    #     group before: [:foo] do
+    #       get :baz, "/baz" do
+    #         # "foo" has been logged
+    #         send "baz"
+    #       end
+    #     end
+    #   end
+    #
+    # @example Building a path to a route within a named group:
+    #   path :foo_bar
+    #   => "/foo/bar"
+    #
+    # @example Building a path to a route within an unnamed group:
+    #   path :foo_baz
+    #   => nil
+    #
+    #   path :baz
+    #   => "/baz"
+    #
+    def group(name = nil, **hooks, &block)
+      router = Router.new(name, **compile_hooks(hooks))
+      router.instance_eval(&block)
+      children << router
+    end
+
+    # Creates a group of routes and mounts them at a path, with an optional
+    # name as well as hooks. A namespace behaves just like a group with
+    # regard to path lookup and hook inheritance.
+    #
+    # @example Defining a namespace:
+    #   Pakyow::Router.new do
+    #     namespace :api, "/api" do
+    #       func :auth do
+    #         handle 401 unless authed?
+    #       end
+    #
+    #       group before: [:auth] do
+    #         namespace :project, "/projects" do
+    #           get :list, "/" do
+    #             # route is accessible via 'GET /api/projects'
+    #             send projects.to_json
+    #           end
+    #         end
+    #       end
+    #     end
+    #   end
+    #
+    def namespace(name_or_path = nil, path_or_name, **hooks, &block)
+      # TODO: support regex path
+      args  = Aargv.normalize([name_or_path, path_or_name], name: Symbol, path: String)
+      name, path = args.values_at(:name, :path)
+
+      router = Router.new(name, full_path(path), **compile_hooks(hooks))
+      router.instance_eval(&block)
+      children << router
+    end
+
+    # TODO: rename to endpoint
+    def path_to(*names, **params)
+      first_name = names.first
+      if route = routes.values.flatten.find { |route| route.name == first_name }
+        if route.parameterized?
+          return route.populated_path(**params)
+        else
+          return route.path
         end
-      }
+      end
+
+      children.reject { |router_to_match|
+        router_to_match.name.nil? || router_to_match.name != first_name
+      }.each do |matched_router|
+        if path = matched_router.path_to(*names[1..-1], **params)
+          return path
+        end
+      end
+    end
+
+    # Creates a route template with a name and block. The block is
+    # evaluated within a {Routing::Expansion} instance when / if it
+    # is later expanded at some endpoint (creating a namespace).
+    #
+    # Route templates are used to define a scaffold of default routes
+    # that will later be expanded at some path. During expansion, the
+    # scaffolded routes are also mapped to routing logic.
+    #
+    # Because routes can be referenced by name during expansion, route
+    # templates provide a way to create a domain-specific-language, or
+    # DSL, around a routing concern. This is used within Pakyow itself
+    # to define the resource template ({Routing::Extensions::Resource}).
+    #
+    # @example Defining a template:
+    #   Pakyow::Router.new do
+    #     template :talkback do
+    #       get :hello, "/hello"
+    #       get :goodbye, "/goodbye"
+    #     end
+    #   end
+    #
+    # @example Expanding a template:
+    #
+    #   Pakyow::Router.new do
+    #     talkback :en, "/en" do
+    #       hello do
+    #         send "hello"
+    #       end
+    #
+    #       goodbye do
+    #         send "goodbye"
+    #       end
+    #
+    #       # we can also extend the expansion
+    #       # for our particular use-case
+    #       get "/thanks" do
+    #         send "thanks"
+    #       end
+    #     end
+    #
+    #     talkback :fr, "/fr" do
+    #       hello do
+    #         send "bonjour"
+    #       end
+    #
+    #       # `goodbye` will not be an endpoint
+    #       # since we did not expand it here
+    #     end
+    #   end
+    #
+    def template(name, &block)
+      templates[name] = block
+    end
+
+    # TODO: flesh this out
+    def expand
+    end
+
+    # TODO: call the `expand` method
+    def method_missing(method, *args, **hooks, &block)
+      if template = templates[method]
+        args[1] = full_path(args[1] || "")
+        expansion = Routing::Expansion.new(template, *args, **hooks, &block)
+        children << expansion.router
+      else
+        raise NameError, "Unknown template `#{method}'"
+      end
+    end
+
+    def call(path, method, request: nil, context: nil)
+      path = String.normalize_path(path)
+
+      routes[method].each do |route|
+        catch :reject do
+          next unless route.match?(path, request)
+          route.call(context: context)
+          return true
+        end
+      end
+
+      children.each do |child_router|
+        return true if child_router.call(path, method, request: request, context: context) === true
+      end
+    end
+
+    # @api private
+    def merge(router)
+      merge_funcs(router.funcs)
+      merge_hooks(router.hooks)
+      merge_routes(router.routes)
+      merge_templates(router.templates)
+    end
+
+    # api private
+    def freeze
+      hooks.each do |_, hooks_arr|
+        hooks_arr.freeze
+      end
+
+      children.each do |child|
+        child.freeze
+      end
+
+      routes.each do |_, routes_arr|
+        routes_arr.each(&:freeze)
+        routes_arr.freeze
+      end
+
+      path.freeze
+      funcs.freeze
+      hooks.freeze
+      children.freeze
+      routes.freeze
+      templates.freeze
+
+      super
     end
 
     protected
 
-    # Calls a list of route functions in order (in a shared context).
-    #
-    def call_fns(fns, app)
-      fns.each {|fn| app.instance_exec(&fn)}
+    def build_route(method, name_or_path, path_or_name, func: nil, **hooks, &block)
+      args  = Aargv.normalize([name_or_path, path_or_name], name: Symbol, path: String, regex: Regexp)
+      block = func(func) if func
+
+      route = Routing::Route.new(
+        name: args[:name],
+        path: full_path(args[:path] || args[:regex]),
+        hooks: compile_hooks(hooks || {}),
+        &block
+      )
+
+      routes[method] << route
+      route
     end
 
-    # Finds the first matching route for the request path/method and
-    # returns the list of route functions for that route.
-    #
-    def match(request)
-      path   = String.normalize_path(request.path)
-      method = request.method
-
-      match, data = nil
-      @sets.each { |set|
-        match, data = set[1].match(path, method)
-        break if match
-      }
-
-      fns = []
-      if match
-        fns = match[3]
-
-        # handle route params
-        #TODO where to do this?
-        request.params.merge!(Hash.strhash(self.data_from_path(path, data, match[1])))
-
-        #TODO where to do this?
-        request.route_path = match[4]
+    def full_path(path_part)
+      path = @nested_path || @path
+      if path.is_a?(Regexp) || path_part.is_a?(Regexp)
+        Regexp.new("^#{File.join(path.to_s, path_part.to_s)}$")
+      else
+        File.join(path.to_s, path_part.to_s)
       end
-
-      fns
     end
 
-    # Calls route functions and catches new functions as
-    # they're thrown (e.g. by reroute).
-    #
-    def trampoline(fns, app)
-      routed = false
-      until fns.empty?
-        fns = catch(:fns) {
-          self.call_fns(fns, app)
+    def compile_hooks(hooks_to_compile)
+      # TODO: seems weird that we'd call `combined_hooks` here
+      combined = combined_hooks(hooks_to_compile)
 
-          # Getting here means that call() returned normally (not via a throw)
-          :fall_through
-        } # end :fns catch block
-
-        # If reroute! or invoke_handler! was called in the block, block will have a new value (nil or block).
-        # If neither was called, block will be :fall_through
-
-        routed = case fns
-          when []             then false
-          when :fall_through  then fns = [] and true
-        end
+      combined.each do |type, hooks|
+        combined[type] = hooks.map { |hook|
+          hook.is_a?(Proc) ? hook : func(hook)
+        }
       end
-
-      return routed
     end
 
-    # Extracts the data from a path.
-    #
-    def data_from_path(path, matches, vars)
-      data = {}
-      return data unless matches
+    def combined_hooks(hooks_to_combine)
+      hooks.each_with_object({}) do |(type, hooks), combined|
+        combined[type] = hooks.dup.concat(
+          Array.ensure(hooks_to_combine[type] || [])
+        ).uniq
+      end
+    end
 
-      matches.names.each {|var|
-        data[var.to_sym] = matches[var]
-      }
+    # TODO: move these merge funcs somewhere else
+    def merge_funcs(funcs_to_merge)
+      funcs.merge!(funcs_to_merge)
+    end
 
-      data
+    def merge_hooks(hooks_to_merge)
+      hooks.each_pair do |type, hooks_of_type|
+        hooks_of_type.concat(hooks_to_merge[type] || [])
+      end
+    end
+
+    def merge_routes(routes_to_merge)
+      routes.each_pair do |type, routes_of_type|
+        routes_of_type.concat(routes_to_merge[type].map { |route_to_merge|
+          route = route_to_merge.dup
+          route.instance_variable_set(:@path, String.normalize_path(full_path(route_to_merge.path)))
+          route
+        })
+      end
+    end
+
+    def merge_templates(templates_to_merge)
+      templates.merge!(templates_to_merge)
     end
   end
 end
---
+++ pakyow-core/lib/pakyow/core/routing/route.rb
@@ -0,0 +1,109 @@
+module Pakyow
+  module Routing
+    class Route
+      attr_reader :name, :path, :parameterized_path, :block, :hooks, :pipeline
+
+      def initialize(name: nil, path: nil, hooks: nil, &block)
+        @name     = name
+        @path     = path
+        @block    = block
+        @hooks    = hooks
+        @pipeline = compile_pipeline(block, hooks)
+
+        if @path.is_a?(String)
+          @path = String.normalize_path(path)
+          parameterize_path
+        end
+      end
+
+      def match?(path_to_match, request)
+        case path
+        when Regexp
+          if data = path.match(path_to_match)
+            request.params.merge!(Hash[data.names.zip(data.captures)])
+            true
+          end
+        when String
+          path == path_to_match
+        else
+          false
+        end
+      end
+
+      def call(context: nil)
+        if context.nil?
+          @pipeline.each(&:call)
+        else
+          @pipeline.each do |route|
+            context.instance_exec(&route)
+          end
+        end
+      end
+
+      def populated_path(**params)
+        parameterized_path.split("/").map { |path_segment|
+          if path_segment[0] == ":"
+            params[path_segment[1..-1].to_sym]
+          else
+            path_segment
+          end
+        }.join("/")
+      end
+
+      def recompile(block: nil, hooks: nil)
+        @block = block if block
+        @hooks = merge_hooks(@hooks, hooks) if hooks
+        @pipeline = compile_pipeline(@block, @hooks)
+      end
+
+      def parameterized?
+        !@parameterized_path.nil?
+      end
+
+      def freeze
+        path.freeze
+        pipeline.freeze
+        # TODO: freeze hooks
+        super
+      end
+
+      protected
+
+      # TODO: move into Routing::Helpers
+      def compile_pipeline(block, hooks)
+        [
+          hooks[:around],
+          hooks[:before],
+          block,
+          hooks[:after],
+          hooks[:around]
+        ].flatten.compact
+      end
+
+      # TODO: move into Routing::Helpers
+      def merge_hooks(hooks, hooks_to_merge)
+        hooks.each_pair do |type, hooks_of_type|
+          hooks_of_type.concat(hooks_to_merge[type] || [])
+        end
+      end
+
+      def parameterize_path
+        if @path.include?(":")
+          # replace named parameters with a named capture
+          reqex_path = @path.split("/").map { |segment|
+            if segment.include?(":")
+              '(?<' + segment[1..-1] + '>(\w|[-.~:@!$\'\(\)\*\+,;])*)'
+            else
+              segment
+            end
+          }.join("/")
+
+          @parameterized_path = @path
+
+          # perform the actual matching via regex
+          @path = Regexp.new("^#{reqex_path}$")
+        end
+      end
+    end
+  end
+end
