b19549e39c6cdab89e77698292400af8ee479d90
Bryan Powell <bryan@metabahn.com>
2017-03-04 10:52:21 -0800

    Remove empty errors file

--- pakyow-core/lib/pakyow/core.rb
+++ pakyow-core/lib/pakyow/core.rb
@@ -4,7 +4,6 @@ require "pakyow/support"
 require "pakyow/core/app"
 require "pakyow/core/controller"
 require "pakyow/core/helpers"
-require "pakyow/core/errors"
 
 require "pakyow/core/router"
 require "pakyow/core/routing/route"
