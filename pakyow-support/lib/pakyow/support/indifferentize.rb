34fa0248d1eeeddf8ffaeef328b9ad32cc637dfa
jphager2 <jphager2@gmail.com>
2017-03-07 14:17:54 +0100

    make Pakyow::Support::IndifferentHash respect Hash interface

--- pakyow-support/lib/pakyow/support/indifferentize.rb
+++ pakyow-support/lib/pakyow/support/indifferentize.rb
@@ -1,19 +1,102 @@
 module Pakyow
   module Support
-    class IndifferentHash < Hash
-      def [](key)
-        super(key.to_s)
+    # Creates a Hash-like object can access stored data with symbol or
+    #   string keys.
+    #
+    # The original hash is converted to symbol keys, which means that a hash
+    #   that originally contains a symbol and string key with the same to_sym
+    #   value will conflict. It is not guaranteed which value will be saved.
+    #
+    # @example
+    #   { test: 'test1', 'test' => 'test2' } => { test: 'test2' }
+    #
+    class IndifferentHash < SimpleDelegator
+      class << self
+        def deep(hash)
+          pairs = hash.each_pair.map do |key, value|
+            if value.is_a? Hash
+              value = deep(value)
+            end
+            [key, value]
+          end
+
+          self.new(Hash[pairs])
+        end
+
+        def indifferent_key_method(*methods)
+          methods.each do |name|
+            define_method(name) do |key = nil, *args, &block|
+              key = key.to_sym if key.respond_to?(:to_sym)
+              internal_hash.public_send(name, key, *args, &block)
+            end
+          end
+        end
+
+        def indifferent_multi_key_method(*methods)
+          methods.each do |name|
+            define_method(name) do |*keys, &block|
+              keys = keys.map do |key|
+                key.to_sym if key.respond_to?(:to_sym)
+              end
+              internal_hash.public_send(name, *keys, &block)
+            end
+          end
+        end
+
+        def indifferentize_method(*methods)
+          methods.each do |name|
+            define_method(name) do |*args, &block|
+              hash = internal_hash.public_send(name, *args, &block)
+              self.class.new(hash)
+            end
+          end
+        end
+
+        def indifferentize_update_method(*methods)
+          methods.each do |name|
+            define_method(name) do |hash, &block|
+              hash = symbolize_keys(hash)
+              internal_hash.public_send(name, hash, &block)
+            end
+          end
+        end
       end
 
-      def []=(key, value)
-        super(key.to_s, value)
+      def initialize(hash)
+        self.internal_hash = hash
+      end
+
+      indifferent_key_method :[], :[]=, :default, :delete, :dig, :fetch, :has_key?, :key?, :include?, :member?, :store
+      indifferent_multi_key_method :fetch_values, :values_at
+      indifferentize_method :merge, :invert
+      indifferentize_update_method :merge!, :update, :replace
+      
+      private
+
+      def internal_hash
+        __getobj__
+      end
+
+      def internal_hash=(other)
+        __setobj__(symbolize_keys(other))
+      end
+
+      def symbolize_keys(hash)
+        hash.each_with_object({}) do |(key, value), converted|
+          key = key.to_sym if key.respond_to?(:to_sym)
+          converted[key] = value
+        end
       end
     end
 
     module Indifferentize
       refine Hash do
         def indifferentize
-          Pakyow::Support::IndifferentHash[self]
+          Pakyow::Support::IndifferentHash.new(self)
+        end
+
+        def deep_indifferentize
+          Pakyow::Support::IndifferentHash.deep(self)
         end
       end
     end
