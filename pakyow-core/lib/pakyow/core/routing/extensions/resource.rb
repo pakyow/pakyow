7bb928f0b6524b7eee1bdfcee3b44c421bb83d4f
Bryan Powell <bryan@metabahn.com>
2017-03-14 20:42:14 -0700

    Improve Pakyow::Routing::Extension::Resource docs

--- pakyow-core/lib/pakyow/core/routing/extensions/resource.rb
+++ pakyow-core/lib/pakyow/core/routing/extensions/resource.rb
@@ -3,6 +3,74 @@ require "pakyow/core/routing/extension"
 module Pakyow
   module Routing
     module Extension
+      # An extension for defining RESTful Resources. For example:
+      #
+      #   Pakyow::App.router do
+      #     resource :post, "/posts" do
+      #       list do
+      #         # list the posts
+      #       end
+      #     end
+      #   end
+      #
+      # +Resource+ is available in all routers by default.
+      #
+      # = Supported Actions
+      #
+      # These actions are supported:
+      #
+      # - +list+ -- +GET /+
+      # - +new+ -- +GET /new+
+      # - +create+ -- +POST /+
+      # - +edit+ -- +GET /:resource_id/edit+
+      # - +update+ -- +PATCH /:resource_id+
+      # - +replace+ -- +PUT /:resource_id+
+      # - +remove+ -- +DELETE /:resource_id+
+      # - +show+ -- +GET /:resource_id+
+      #
+      # = Nested Resources
+      #
+      # Resources can be nested. For example:
+      #
+      #   Pakyow::App.router do
+      #     resource :post, "/posts" do
+      #       resource :comment, "/comments" do
+      #         list do
+      #           # available at GET /posts/:post_id/comments
+      #         end
+      #       end
+      #     end
+      #   end
+      #
+      # = Collection Routes
+      #
+      # Routes can be defined for the collection. For example:
+      #
+      #   Pakyow::App.router do
+      #     resource :post, "/posts" do
+      #       collection do
+      #         get "/foo" do
+      #           # available at GET /posts/foo
+      #         end
+      #       end
+      #     end
+      #   end
+      #
+      # = Member Routes
+      #
+      # Routes can be defined as members. For example:
+      #
+      #   Pakyow::App.router do
+      #     resource :post, "/posts" do
+      #       member do
+      #         get "/foo" do
+      #           # available at GET /posts/:post_id/foo
+      #         end
+      #       end
+      #     end
+      #   end
+      #
+      # @api public
       module Resource
         include Extension
 
