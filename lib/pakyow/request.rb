e890253de9fe7ae334b1c036b1436e20c7684d97
Bryan Powell <bryan@metabahn.com>
2017-03-21 12:30:54 -0700

    Refactor Request#format to be based on path ext

--- lib/pakyow/request.rb
+++ lib/pakyow/request.rb
@@ -34,15 +34,13 @@ module Pakyow
     #
     # @api public
     def format
-      type = Rack::Mime::MIME_TYPES.select { |_key, value|
-        value == @env["CONTENT_TYPE"]
-      }
+      return @format if defined?(@format)
 
-      return if type.empty?
-      extension = type.keys.first
-      # works around a dumb thing in Rack::Mime
-      return :html if extension == ".htm"
-      extension[1..-1].to_sym
+      if path.include?(".")
+        @format = path.split(".").last.to_sym
+      else
+        @format = :html
+      end
     end
 
     # Returns an indifferentized params hash.
--- spec/unit/request_spec.rb
+++ spec/unit/request_spec.rb
@@ -58,9 +58,9 @@ RSpec.describe Pakyow::Request do
   end
 
   describe "#format" do
-    context "when content type is text/html" do
+    context "when request format is unspecified" do
       before do
-        request.env["CONTENT_TYPE"] = "text/html"
+        request.env["PATH_INFO"] = "foo"
       end
 
       it "returns the symbolized format" do
@@ -68,9 +68,9 @@ RSpec.describe Pakyow::Request do
       end
     end
 
-    context "when content type is application/json" do
+    context "when request format is json" do
       before do
-        request.env["CONTENT_TYPE"] = "application/json"
+        request.env["PATH_INFO"] = "foo.json"
       end
 
       it "returns the symbolized format" do
@@ -80,11 +80,11 @@ RSpec.describe Pakyow::Request do
 
     context "when content type is unknown" do
       before do
-        request.env["CONTENT_TYPE"] = "foo/bar"
+        request.env["PATH_INFO"] = "foo.foobar"
       end
 
       it "returns the symbolized format" do
-        expect(request.format).to eq(nil)
+        expect(request.format).to eq(:foobar)
       end
     end
   end
