ceacca9d2b73ae99fddc3eba6d8dfb0be6d864fa
Bryan Powell <bryan@metabahn.com>
2017-06-25 16:52:42 -0700

    Refactor string doc parser to be more legible

--- pakyow-presenter/lib/pakyow/presenter/string_doc_parser.rb
+++ pakyow-presenter/lib/pakyow/presenter/string_doc_parser.rb
@@ -5,9 +5,40 @@ module Pakyow
     class StringDocParser
       include Support::Silenceable
 
-      PARTIAL_REGEX = /<!--\s*@include\s*([a-zA-Z0-9\-_]*)\s*-->/
-      CONTAINER_REGEX = /@container( ([a-zA-Z0-9\-_]*))*/
-      SIGNIFICANT = [:scope?, :prop?, :container?, :partial?, :option?, :component?]
+      PARTIAL_REGEX = /@include\s*([a-zA-Z0-9\-_]*)/.freeze
+      CONTAINER_REGEX = /@container\s*([a-zA-Z0-9\-_]*)/.freeze
+
+      def self.significant(name, &block)
+        significant_types[name] = block
+      end
+
+      def self.significant_types
+        @significant_types ||= {}
+      end
+
+      significant :scope do |node|
+        node.is_a?(Oga::XML::Element) && node.attribute('data-scope')
+      end
+
+      significant :prop do |node|
+        node.is_a?(Oga::XML::Element) && node.attribute('data-prop')
+      end
+
+      significant :container do |node|
+        node.is_a?(Oga::XML::Comment) && node.text.strip.match(CONTAINER_REGEX)
+      end
+
+      significant :partial do |node|
+        node.is_a?(Oga::XML::Comment) && node.to_xml.strip.match(PARTIAL_REGEX)
+      end
+
+      significant :option do |node|
+        node.is_a?(Oga::XML::Element) && node.name == 'option'
+      end
+
+      significant :component do |node|
+        node.is_a?(Oga::XML::Element) && node.attribute('data-ui')
+      end
 
       attr_reader :structure
 
@@ -17,7 +48,7 @@ module Pakyow
         end
       end
 
-      private
+      protected
 
       # Parses HTML and returns a nested structure representing the document.
       #
@@ -25,123 +56,107 @@ module Pakyow
         structure = []
 
         unless doc.is_a?(Oga::XML::Element) || !doc.respond_to?(:doctype) || doc.doctype.nil?
-          structure << ['<!DOCTYPE html>', {}, []]
+          structure << ["<!DOCTYPE html>", {}, []]
         end
 
         breadth_first(doc) do |node, queue|
           if node == doc
-            queue.concat(node.children.to_a)
-            next
+            queue.concat(node.children.to_a); next
           end
 
+          # TODO: why do we do this? optimization?
           children = node.children.reject {|n| n.is_a?(Oga::XML::Text)}
 
-          if node.is_a?(Oga::XML::Element)
-            attributes = node.attributes
-          else
-            attributes = []
+          # this is an optimization we can make because we don't care about this node and
+          # we know that nothing inside of it is significant, so we can just collapse it
+          if !structure.empty? && children.empty? && !significant?(node)
+            structure << [node.to_xml, {}, []]; next
           end
 
-          if !structure.empty? && children.empty? && !significant?(node)
-            structure << [node.to_xml, {}, []]
-          else
-            if significant?(node)
-              if container?(node)
-                match = node.text.strip.match(CONTAINER_REGEX)
-                name = (match[2] || :default).to_sym
-                structure << [node.to_xml, { container: name }, []]
-              elsif partial?(node)
-                next unless match = node.to_xml.strip.match(PARTIAL_REGEX)
-                name = match[1].to_sym
-                structure << [node.to_xml, { partial: name }, []]
-              else
-                attr_structure = attributes.inject({}) do |attrs, attr|
-                  attrs[attr.name.to_sym] = attr.value
-                  attrs
-                end
-
-                closing = [['>', {}, parse(node)]]
-                closing << ["</#{node.name}>", {}, []] unless self_closing?(node.name)
-                structure << ["<#{node.name} ", attr_structure, closing]
-              end
+          if significant?(node)
+            case significance(node)
+            when :container
+              structure << [node.to_xml, { container: container_name(node) }, []]
+            when :partial
+              structure << [node.to_xml, { partial: partial_name(node) }, []]
             else
-              if node.is_a?(Oga::XML::Text) || node.is_a?(Oga::XML::Comment)
-                structure << [node.to_xml, {}, []]
-              else
-                attr_s = attributes.inject('') { |s, a| s << " #{a.name}=\"#{a.value}\""; s }
-                closing = [['>', {}, parse(node)]]
-                closing << ['</' + node.name + '>', {}, []] unless self_closing?(node.name)
-                structure << ['<' + node.name + attr_s, {}, closing]
-              end
+              structure << ["<#{node.name} ", attributes_hash(node), close(node)]
+            end
+          else # insignificant
+            if node.is_a?(Oga::XML::Text) || node.is_a?(Oga::XML::Comment)
+              structure << [node.to_xml, {}, []]
+            else
+              structure << ["<#{node.name}#{attributes_string(node)}", {}, close(node)]
             end
           end
         end
 
-        return structure
-      end
-
-      def significant?(node)
-        SIGNIFICANT.each do |method|
-          return true if send(method, node)
-        end
-
-        false
-      end
-
-      def scope?(node)
-        return false unless node.is_a?(Oga::XML::Element)
-        return false unless node.attribute('data-scope')
-        return true
-      end
-
-      def prop?(node)
-        return false unless node.is_a?(Oga::XML::Element)
-        return false unless node.attribute('data-prop')
-        return true
-      end
-
-      def container?(node)
-        return false unless node.is_a?(Oga::XML::Comment)
-        return false unless node.text.strip.match(CONTAINER_REGEX)
-        return true
-      end
-
-      def partial?(node)
-        return false unless node.is_a?(Oga::XML::Comment)
-        return false unless node.to_xml.strip.match(PARTIAL_REGEX)
-        return true
-      end
-
-      def option?(node)
-        return false unless node.is_a?(Oga::XML::Element)
-        node.name == 'option'
-      end
-
-      def component?(node)
-        return false unless node.is_a?(Oga::XML::Element)
-        return false unless node.attribute('data-ui')
-        return true
-      end
-
-      def breadth_first(doc)
-        queue = [doc]
-        until queue.empty?
-          catch(:reject) do
-            node = queue.shift
-            yield node, queue
-          end
-        end
+        structure
       end
 
       def doc_from_string(string)
         Oga.parse_html(string)
       end
 
-      SELF_CLOSING = %w[area base basefont br hr input img link meta]
-      def self_closing?(tag)
-        SELF_CLOSING.include? tag
+      def breadth_first(doc)
+        queue = [doc]
+        until queue.empty?
+          catch :reject do
+            node = queue.shift
+            yield node, queue
+          end
+        end
       end
 
+      def significant?(node)
+        self.class.significant_types.each do |name, block|
+          return true if block.call(node)
+        end
+
+        false
+      end
+
+      def significance(node)
+        self.class.significant_types.each do |name, block|
+          return name if block.call(node)
+        end
+
+        nil
+      end
+
+      def container_name(node)
+        (node.text.strip.match(CONTAINER_REGEX)[2] || :default).to_sym
+      end
+
+      def partial_name(node)
+        node.text.strip.match(PARTIAL_REGEX)[1].to_sym
+      end
+
+      def attributes(node)
+        if node.is_a?(Oga::XML::Element)
+          node.attributes
+        else
+          []
+        end
+      end
+
+      def attributes_hash(node)
+        attributes(node).each_with_object({}) do |attribute, structure|
+          structure[attribute.name.to_sym] = attribute.value
+        end
+      end
+
+      def attributes_string(node)
+        attributes(node).each_with_object("") do |attribute, string|
+          string << " #{attribute.name}=\"#{attribute.value}\""
+        end
+      end
+
+      def close(node)
+        closing = [[">", {}, parse(node)]]
+        closing << ["</#{node.name}>", {}, []] unless DocHelpers.self_closing_tag?(node.name)
+        closing
+      end
     end
   end
 end
