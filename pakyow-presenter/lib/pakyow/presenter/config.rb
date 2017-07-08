0d6659ffb5364cd0bd3526e65e4d6d37454a40ee
Bryan Powell <bryan@metabahn.com>
2017-07-08 11:30:04 -0700

    Remove unused config settings

--- pakyow-presenter/lib/pakyow/presenter/config.rb
+++ pakyow-presenter/lib/pakyow/presenter/config.rb
@@ -4,20 +4,8 @@ Pakyow::App.settings_for :presenter do
     File.join(config.app.root, "app", "presentation")
   end
 
-  # the attribute expected for scope definitions
-  # TODO: this shouldn't be configurable
-  setting :scope_attribute, "data-scope"
-
-  # the attribute expected for prop definitions
-  # TODO: this also shouldn't be configurable
-  setting :prop_attribute, "data-prop"
-
   # if true, views are visible without a route defined
   setting :require_route, true
-
-  # the document class used to parse and render views
-  # TODO: we won't make this configurable
-  setting :view_doc_class, Pakyow::Presenter::StringDoc
 
   defaults :prototype do
     setting :require_route, false
