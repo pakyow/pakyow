466f16bdc4447694588fd482d684c5dc61c1ff34
Bryan Powell <bryan@metabahn.com>
2017-07-08 11:28:56 -0700

    Require route for view rendering everywhere but prototype

--- pakyow-presenter/lib/pakyow/presenter/config.rb
+++ pakyow-presenter/lib/pakyow/presenter/config.rb
@@ -13,19 +13,13 @@ Pakyow::App.settings_for :presenter do
   setting :prop_attribute, "data-prop"
 
   # if true, views are visible without a route defined
-  # TODO: we should reconsider this difference between dev/prd
-  #   and potentially handle this with a development-only handler for convenience
-  setting :require_route, false
+  setting :require_route, true
 
   # the document class used to parse and render views
   # TODO: we won't make this configurable
   setting :view_doc_class, Pakyow::Presenter::StringDoc
 
-  defaults :test do
-    setting :require_route, true
-  end
-
-  defaults :production do
-    setting :require_route, true
+  defaults :prototype do
+    setting :require_route, false
   end
 end
