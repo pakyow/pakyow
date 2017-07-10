013da37733d51ea2719ef8c4c1f904e9ad750987
Bryan Powell <bryan@metabahn.com>
2017-07-09 22:07:30 -0700

    Catch up the example with the presenter refactor

--- example/app/lib/routes.rb
+++ example/app/lib/routes.rb
@@ -1,7 +1,35 @@
-Pakyow::App.router do
+module Pakyow::Helpers
+  def current_user
+  end
+end
+
+Pakyow::App.router :default do
+  presentable :current_user
+
+  def post
+    [{ body: "foo" }, { body: "bar" }]
+  end
+
   default do
-    render "/" do |view|
-      view.title = "hello world"
+    presentable :post
+
+    # render "/"
+  end
+end
+
+Pakyow::App.view "/" do
+  # view.scope(:post).apply([{ body: rand.to_s }, { body: rand.to_s }, { body: rand.to_s }])
+  find(:post).present(post)
+end
+
+Pakyow::App.binder :post do
+  def body
+    part :content do
+      object[:body].reverse
+    end
+
+    part :title do
+      "woot"
     end
   end
 end
