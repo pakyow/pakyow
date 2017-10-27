9e0481e3682974d6504c711b77006cf2a32a61b5
Bryan Powell <bryan@metabahn.com>
2017-10-27 08:17:38 -0700

    Delete old presenter specs

--- pakyow-presenter/spec.old/integration/support/helpers/view_collection_helpers.rb
+++
@@ -1,6 +0,0 @@
-module ViewCollectionHelpers
-  def view_and_collection
-    yield(View.new)
-    yield(ViewCollection.new)
-  end
-end
--- pakyow-presenter/spec.old/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'rspec'
-require 'pry'
-require 'pp'
-
-require File.expand_path('../../../pakyow-support/lib/pakyow-support', __FILE__)
-require File.expand_path('../../../pakyow-core/lib/pakyow/core', __FILE__)
-require File.expand_path('../../lib/pakyow-presenter', __FILE__)
