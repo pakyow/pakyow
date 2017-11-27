243fd9db7a0b20eca651ae31ba475c15470ba2e5
Bryan Powell <bryan@metabahn.com>
2017-11-27 11:38:03 -0800

    Inline verification

--- pakyow-data/lib/pakyow/data/ext/pakyow/core/router.rb
+++ pakyow-data/lib/pakyow/data/ext/pakyow/core/router.rb
@@ -3,5 +3,18 @@
 module Pakyow
   class Router
     def_delegators :controller, :data, :verify
+
+    class << self
+      # Perform input verification before one or more routes, identified by name.
+      #
+      # @see Pakyow::Data::Verifier
+      #
+      # @api public
+      def verify(*names, &block)
+        before *names do
+          verify &block
+        end
+      end
+    end
   end
 end
