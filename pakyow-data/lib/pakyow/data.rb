2c76fa9f9e9a6795d4db6fe191e053ff292d56d0
Bryan Powell <bryan@metabahn.com>
2017-11-28 12:43:52 -0800

    Load data as a concern

--- pakyow-data/lib/pakyow/data.rb
+++ pakyow-data/lib/pakyow/data.rb
@@ -8,6 +8,37 @@ require "rom"
 module Pakyow
   module Data
     CONNECTION_TYPES = %i(sql memory)
+
+    def self.included(base)
+      load_into(base)
+    end
+
+    def self.load_into(app_class)
+      app_class.class_eval do
+        concern :data
+
+        stateful :model, Pakyow::Data::Model
+
+        attr_reader :data_model_lookup
+
+        before :freeze do
+          models = state_for(:model).each_with_object({}) { |model, models_by_name|
+            next if model.attributes.empty?
+
+            models_by_name[model.name] = model
+          }
+
+          @data_model_lookup = Data::Lookup.new(
+            models,
+            Data::SubscriberStore.new(
+              config.app.name,
+              Pakyow.config.data.subscription_adapter,
+              Pakyow.config.data.subscription_adapter_options
+            )
+          )
+        end
+      end
+    end
   end
 end
 
@@ -22,6 +53,7 @@ require "pakyow/data/validations"
 require "pakyow/data/errors"
 
 require "pakyow/data/ext/pakyow/environment"
-require "pakyow/data/ext/pakyow/core/app"
 require "pakyow/data/ext/pakyow/core/controller"
 require "pakyow/data/ext/pakyow/core/router"
+
+Pakyow.register_framework :data, Pakyow::Data
