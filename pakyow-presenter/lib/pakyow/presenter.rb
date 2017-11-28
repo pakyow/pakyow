8da9e0e1bed834918b17a11dd973c08dfb392f88
Bryan Powell <bryan@metabahn.com>
2017-11-28 12:51:19 -0800

    Load presenter app extensions dynamically

--- pakyow-presenter/lib/pakyow/presenter.rb
+++ pakyow-presenter/lib/pakyow/presenter.rb
@@ -31,7 +31,6 @@ require "pakyow/presenter/views/partial"
 require "pakyow/presenter/presenters/form"
 require "pakyow/presenter/presenters/view"
 
-require "pakyow/presenter/extensions/app"
 require "pakyow/presenter/extensions/controller"
 require "pakyow/presenter/extensions/router"
 
--- pakyow-presenter/lib/pakyow/presenter/presenter.rb
+++ pakyow-presenter/lib/pakyow/presenter/presenter.rb
@@ -7,35 +7,54 @@ module Pakyow
     end
 
     def self.load_presenter_into(app_class)
-      app_class.after :load do
-        app_class.template_store << TemplateStore.new(:default, config.presenter.path, processor: ProcessorCaller.new(app_class.state[:processor].instances))
 
-        if environment == :development
-          app_class.handle Pakyow::Presenter::MissingView, as: 500 do
+      app_class.class_eval do
+        stateful :template_store, TemplateStore
+        stateful :view, ViewPresenter
+        stateful :binder, Binder
+        stateful :processor, Processor
+
+        settings_for :presenter do
+          setting :path do
+            File.join(config.app.root, "interface")
+          end
+
+          setting :require_route, false
+        end
+
+        concern :views
+        concern :binders
+
+        after :load do
+          app_class.template_store << TemplateStore.new(:default, config.presenter.path, processor: ProcessorCaller.new(app_class.state[:processor].instances))
+
+          if environment == :development
+            app_class.handle MissingView, as: 500 do
+              respond_to :html do
+                render "/missing_view"
+              end
+            end
+
+            app_class.template_store << TemplateStore.new(:errors, File.join(File.expand_path("../../", __FILE__), "views", "errors"))
+
+            # TODO: define view objects to render built-in errors
+          end
+
+          # TODO: the following handlers override the ones defined on the app
+          # ideally global handlers could coexist (e.g. handle bugsnag, then present error page)
+          # perhaps by executing all of 'em at once until halted or all called; feels consistent with
+          # how multiple handlers are called in non-global cases; though load order would be important
+
+          app_class.handle 404 do
             respond_to :html do
-              render "/missing_view"
+              render "/404"
             end
           end
 
-          app_class.template_store << TemplateStore.new(:errors, File.join(File.expand_path("../../", __FILE__), "views", "errors"))
-
-          # TODO: define view objects to render built-in errors
-        end
-
-        # TODO: the following handlers override the ones defined on the app
-        # ideally global handlers could coexist (e.g. handle bugsnag, then present error page)
-        # perhaps by executing all of 'em at once until halted or all called; feels consistent with
-        # how multiple handlers are called in non-global cases; though load order would be important
-
-        app_class.handle 404 do
-          respond_to :html do
-            render "/404"
-          end
-        end
-
-        app_class.handle 500 do
-          respond_to :html do
-            render "/500"
+          app_class.handle 500 do
+            respond_to :html do
+              render "/500"
+            end
           end
         end
       end
