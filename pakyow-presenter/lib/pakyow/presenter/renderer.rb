f4a3ed4489485319fed4b924e8b38877eb0444e1
Bryan Powell <bryan@metabahn.com>
2017-12-09 10:52:58 -0800

    Pass presentables to define
    This allows renderer subclasses to define presentables differently.

--- pakyow-presenter/lib/pakyow/presenter/renderer.rb
+++ pakyow-presenter/lib/pakyow/presenter/renderer.rb
@@ -64,7 +64,7 @@ module Pakyow
           **info
         )
 
-        define_presentables
+        define_presentables(@__state.get(:presentables))
 
         performing :render do
           response.body = StringIO.new(
@@ -117,8 +117,8 @@ module Pakyow
         nil
       end
 
-      def define_presentables
-        @__state.get(:presentables)&.each do |name, value|
+      def define_presentables(presentables)
+        presentables&.each do |name, value|
           @current_presenter.define_singleton_method name do
             value
           end
