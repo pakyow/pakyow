c3dc6292ca109e94f27fac1bd2834a1b193a396b
Bryan Powell <bryan@metabahn.com>
2018-01-18 07:49:34 -0800

    Restart webpack when a view changes
    So that packs and partial assets are composed properly.

--- pakyow-assets/lib/pakyow/assets/process.rb
+++ pakyow-assets/lib/pakyow/assets/process.rb
@@ -15,11 +15,24 @@ module Pakyow
         @pid = ::Process.spawn("./node_modules/.bin/webpack --config config/assets/environment.js --watch", out: File.open(File::NULL, "w"), err: $stderr)
       end
 
-      def watch_callback(_modified, added, removed)
-        if (added + removed).find { |path| Assets.extensions.include?(File.extname(path)) }
+      def watch_callback(modified, added, removed)
+        if restart?(modified, added, removed)
           @app.build_packs
           restart
         end
+      end
+
+      def restart?(modified, added, removed)
+        return true if (added + removed).find { |path|
+          Assets.extensions.include?(File.extname(path))
+        }
+
+        expanded_presenter_path = File.expand_path(@app.config.presenter.path)
+        return true if modified.find { |path|
+          !Assets.extensions.include?(File.extname(path)) && File.expand_path(path).start_with?(expanded_presenter_path)
+        }
+
+        false
       end
     end
   end
