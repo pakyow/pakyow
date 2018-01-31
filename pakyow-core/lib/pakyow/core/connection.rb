3cc4657e87efe5804c762b17f3baa98e3fee25ce
Bryan Powell <bryan@metabahn.com>
2018-01-31 12:48:43 -0800

    Add delegator for type

--- pakyow-core/lib/pakyow/core/connection.rb
+++ pakyow-core/lib/pakyow/core/connection.rb
@@ -35,7 +35,7 @@ module Pakyow
     end
 
     extend Forwardable
-    def_delegators :request, :method, :format, :host, :port, :ip, :user_agent, :base_url, :path,
+    def_delegators :request, :method, :format, :type, :host, :port, :ip, :user_agent, :base_url, :path,
                    :path_info, :script_name, :url, :params, :cookies, :session, :env, :logger, :ssl?
 
     def_delegators :response, :status, :status=, :write, :close, :body=
