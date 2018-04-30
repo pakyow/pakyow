f90fd0e3000b7cab3073a29e7fcae7a8343af89e
Bryan Powell <bryan@metabahn.com>
2018-04-30 16:16:57 -0700

    Protect against no values in command

--- pakyow-data/lib/pakyow/data/command.rb
+++ pakyow-data/lib/pakyow/data/command.rb
@@ -12,35 +12,37 @@ module Pakyow
       end
 
       def call(values = nil)
-        if values && timestamp_fields = @source.class.timestamp_fields
-          if @name == :create
-            timestamp_fields.values.each do |timestamp_field|
+        if values
+          if timestamp_fields = @source.class.timestamp_fields
+            if @name == :create
+              timestamp_fields.values.each do |timestamp_field|
+                values[timestamp_field] = Time.now
+              end
+            elsif timestamp_field = timestamp_fields[@name]
               values[timestamp_field] = Time.now
             end
-          elsif timestamp_field = timestamp_fields[@name]
-            values[timestamp_field] = Time.now
-          end
-        end
-
-        if values.is_a?(Support::IndifferentHash)
-          values = values.__getobj__
-        end
-
-        values = values.deep_dup
-
-        @source.class.associations.values.flatten.each do |association|
-          inflector_call = case association[:access_type]
-          when :one then :singularize
-          when :many then :pluralize
           end
 
-          key = Support.inflector.public_send(
-            inflector_call,
-            association[:source_name]
-          ).to_sym
+          if values.is_a?(Support::IndifferentHash)
+            values = values.__getobj__
+          end
 
-          if values.key?(key)
-            values[association[:column_name]] = values.delete(key)[@source.class.primary_key_field]
+          values = values.deep_dup
+
+          @source.class.associations.values.flatten.each do |association|
+            inflector_call = case association[:access_type]
+            when :one then :singularize
+            when :many then :pluralize
+            end
+
+            key = Support.inflector.public_send(
+              inflector_call,
+              association[:source_name]
+            ).to_sym
+
+            if values.key?(key)
+              values[association[:column_name]] = values.delete(key)[@source.class.primary_key_field]
+            end
           end
         end
 
