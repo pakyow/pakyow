5bac9e8b6fa1268875268cb05dc3a79158467003
Bryan Powell <bryan@metabahn.com>
2018-05-18 16:49:12 -0700

    Remove example
    pakyow/design now serves as the canonical example

--- example/public/scripts/ring/components/loader.js
+++
@@ -1,9 +0,0 @@
-pw.component.register('loader', function (view) {
-  this.listen('socket:loading', function () {
-    view.node.classList.add('ui-show');
-  });
-
-  this.listen('socket:loaded', function () {
-    view.node.classList.remove('ui-show');
-  });
-});
