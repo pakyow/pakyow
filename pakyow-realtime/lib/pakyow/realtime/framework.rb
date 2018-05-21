c70c9a4b73b2a1e753c025b2f9d3db04f2f8b662
Bryan Powell <bryan@metabahn.com>
2018-05-21 13:29:13 -0700

    Use connection directly when connecting a websocket

--- pakyow-realtime/lib/pakyow/realtime/framework.rb
+++ pakyow-realtime/lib/pakyow/realtime/framework.rb
@@ -13,10 +13,10 @@ module Pakyow
       def initialize(_)
       end
 
-      def call(state)
+      def call(connection)
         return unless Pakyow.config.realtime.server
-        return unless state.request.path == "/pw-socket"
-        return unless ::WebSocket::Driver.websocket?(state.request.env)
+        return unless connection.path == "/pw-socket"
+        return unless ::WebSocket::Driver.websocket?(connection.env)
 
         # Verify that the websocket is connecting with a valid digest.
         #
@@ -25,13 +25,13 @@ module Pakyow
         # `id:digest` value is embedded in the response, while the key is stored in
         # the session. We verify by generating the digest and comparing it to the
         # digest sent in the connection attempt.
-        id, digest = state.request.params[:id].to_s.split(":", 2)
+        id, digest = connection.params[:id].to_s.split(":", 2)
         return unless Support::MessageVerifier.valid?(
-          id, digest: digest, key: state.request.session[:socket_server_id]
+          id, digest: digest, key: connection.session[:socket_server_id]
         )
 
-        WebSocket.new(id, state)
-        state.halt
+        WebSocket.new(id, connection)
+        connection.halt
       end
     end
 
