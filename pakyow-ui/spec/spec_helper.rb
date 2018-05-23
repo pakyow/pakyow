7c3ac345bd47d922b2cd32e78142754dbfdea145
Bryan Powell <bryan@metabahn.com>
2018-05-23 08:22:29 -0700

    Prevent values from changing in ui specs

--- pakyow-ui/spec/spec_helper.rb
+++ pakyow-ui/spec/spec_helper.rb
@@ -33,12 +33,29 @@ $ui_app_boilerplate = Proc.new do
   end
 end
 
+def process_ui_case_html(html)
+  html.gsub!(/<html data-t=".*">/, "<html data-t=\"61aeb4df92f094a6ae2e1c0a569e480d2244c8fe\">")
+  html.gsub!(/<meta name="pw-authenticity-token" content=".*">/, "<meta name=\"pw-authenticity-token\" content=\"046d2692c570018f1ea143655a284d6b0dcc4c6c292b6f13:pJtz6AYWD+8G5EDcSUgEtbtIK8loL184T2v/8//5dYw=\">")
+  html.gsub!(/<meta name="pw-connection-id" content=".*">/, "<meta name=\"pw-connection-id\" content=\"ece651540c6d53f8ce12833330211991702dfce3b11c50d2:CRaA6BaBQZdUKNHgOgaACEhQbQKOywdaYchDI7O6/jw=\">")
+  html.gsub!(/<input type="hidden" name="authenticity_token" value=".*">/, "<input type=\"hidden\" name=\"authenticity_token\" value=\"046d2692c570018f1ea143655a284d6b0dcc4c6c292b6f13:pJtz6AYWD+8G5EDcSUgEtbtIK8loL184T2v/8//5dYw=\">")
+  HtmlBeautifier.beautify(html)
+end
+
+def process_ui_case_transformations(transformations)
+  transformations = JSON.parse(transformations.to_json)
+  transformations.each do |transformation|
+    transformation["id"] = "61aeb4df92f094a6ae2e1c0a569e480d2244c8fe"
+  end
+
+  JSON.pretty_generate(transformations)
+end
+
 def save_ui_case(example, path:)
   initial_response = call(path)
   expect(initial_response[0]).to eq(200)
   initial = initial_response[2].body.read
 
-  transformation = ws_intercept do
+  transformations = ws_intercept do
     yield
   end
 
@@ -57,15 +74,15 @@ def save_ui_case(example, path:)
 
   File.open(
     File.join(save_path, "initial.html"), "w+"
-  ).write(HtmlBeautifier.beautify(initial))
+  ).write(process_ui_case_html(initial))
 
   File.open(
     File.join(save_path, "result.html"), "w+"
-  ).write(HtmlBeautifier.beautify(result))
+  ).write(process_ui_case_html(result))
 
   File.open(
     File.join(save_path, "transformations.json"), "w+"
-  ).write(JSON.pretty_generate(transformation))
+  ).write(process_ui_case_transformations(transformations))
 
-  transformation
+  transformations
 end
