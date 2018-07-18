134dc1d5b2a082d1b43ebd1c097e52d7595489c9
Bryan Powell <bryan@metabahn.com>
2018-07-17 22:29:20 -0700

    Combine logic for sass/scss

--- pakyow-assets/lib/pakyow/assets/types/sass.rb
+++ pakyow-assets/lib/pakyow/assets/types/sass.rb
@@ -1,51 +1,66 @@
 # frozen_string_literal: true
 
+require "pakyow/support/extension"
+
 require "pakyow/assets/asset"
 
 module Pakyow
   module Assets
     module Types
       class Sass < Asset
-        class << self
-          def load
-            require "sass"
-          rescue LoadError
-            Pakyow.logger.error <<~ERROR
-              Pakyow found a *.sass file, but couldn't find sass. Please add this to your Gemfile:
+        module Behavior
+          extend Support::Extension
 
-                gem "sass"
-            ERROR
+          def initialize(local_path:, config:, **kwargs)
+            @options = {
+              syntax: self.class.const_get("FORMAT"),
+              cache: false,
+              load_paths: [
+                File.dirname(local_path),
+                config.frontend_assets_path
+              ],
+              style: config.minify ? :compressed : :nested
+            }
+
+            super
+          end
+
+          def process(content)
+            ::Sass::Engine.new(content, @options).render
+          rescue ::Sass::SyntaxError => e
+            Pakyow.logger.error "[#{self.class.const_get("FORMAT")}] syntax error: #{e}"
+          end
+
+          def dependencies
+            ::Sass::Engine.for_file(@local_path, @options).dependencies.map { |dependency|
+              dependency.options[:filename]
+            }
+          end
+
+          class_methods do
+            def load
+              unless instance_variable_defined?(:@loaded) && @loaded == true
+                require "sass"
+              end
+            rescue LoadError
+              Pakyow.logger.error <<~ERROR
+                Pakyow found a *.#{const_get("FORMAT")} file, but couldn't find sass. Please add this to your Gemfile:
+
+                  gem "sass"
+              ERROR
+            ensure
+              @loaded = true
+            end
+          end
+
+          apply_extension do
+            extension const_get("FORMAT")
+            emits :css
           end
         end
 
-        extension :sass
-        emits :css
-
-        def initialize(local_path:, config:, **kwargs)
-          @options = {
-            syntax: :sass,
-            cache: false,
-            load_paths: [
-              File.dirname(local_path),
-              config.frontend_assets_path
-            ],
-            style: config.minify ? :compressed : :nested
-          }
-
-          super
-        end
-
-        def process(content)
-          ::Sass::Engine.new(content, @options).render
-        rescue ::Sass::SyntaxError => e
-          Pakyow.logger.error "[sass] syntax error: #{e}"
-        end
-
-        def dependencies
-          ::Sass::Engine.for_file(@local_path, @options).dependencies.map { |dependency|
-            dependency.options[:filename]
-          }
-        end
+        FORMAT = :sass
+        include Behavior
       end
     end
   end
