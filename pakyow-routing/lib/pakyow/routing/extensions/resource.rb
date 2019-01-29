02ff4dc9fdd9a26f108c3672e8eae0cb372bba2b
Bryan Powell <bryan@metabahn.com>
2019-01-29 09:08:22 -0800

    Return the member when defining on a resource

--- pakyow-routing/lib/pakyow/routing/extensions/resource.rb
+++ pakyow-routing/lib/pakyow/routing/extensions/resource.rb
@@ -117,10 +117,11 @@ module Pakyow
           #
           def self.define(controller, nested_resource_id, nested_param)
             controller.define_singleton_method :namespace do |*args, &block|
-              namespace = super(*args, &block)
-              namespace.allow_params nested_param
-              namespace.action :update_request_path_for_parent do
-                req.env["pakyow.endpoint.path"].gsub!(nested_resource_id, "show")
+              super(*args, &block).tap do |namespace|
+                namespace.allow_params nested_param
+                namespace.action :update_request_path_for_parent do
+                  req.env["pakyow.endpoint.path"].gsub!(nested_resource_id, "show")
+                end
               end
             end
 
