842256f34f77ef518374fad2f5e062f901348f87
Bryan Powell <bryan@metabahn.com>
2019-02-06 09:59:08 -0800

    Improve prototype bar styling

--- pakyow-presenter/lib/pakyow/presenter/rendering/actions/insert_prototype_bar.rb
+++ pakyow-presenter/lib/pakyow/presenter/rendering/actions/insert_prototype_bar.rb
@@ -9,12 +9,12 @@ module Pakyow
           if renderer.rendering_prototype?
             if body_node = renderer.presenter.view.object.find_significant_nodes(:body)[0]
               body_node.append <<~HTML
-                <div class="pw-prototype" style="position: fixed; left: 0; right: 0; bottom: 0; width: 100%; height: 20px; background: #111; color: #bbb; text-align: right; font-size: 11px; line-height: 20px">
-                  <div style="background: #777; color: #111; text-transform: uppercase; font-size: 10px; line-height: 20px; padding: 0 10px; float: right">
+                <div class="pw-prototype" style="display:flex; align-items: center; position: fixed; z-index: 1000; right: 0; bottom: 0; background: #156eed; color: #fff; font-size: 11px; line-height: 11px; font-weight: 500; border-top-left-radius: 1px; padding-left: 5px;">
+                  #{ui_modes_html(renderer)}
+
+                  <div style="background: #ff8b6c; color: #fff; text-transform: uppercase; font-size: 10px; line-height: 12px; font-weight: 600; padding: 5px 5px 4px 5px; margin-left: 10px;">
                     Prototype
                   </div>
-
-                  #{ui_modes_html(renderer)}
                 </div>
               HTML
             end
@@ -44,7 +44,7 @@ module Pakyow
           }.join
 
           <<~HTML
-            UI Mode: <select onchange="document.location = window.location.pathname + '?mode=' + this.value " style="-webkit-appearance: none; -moz-appearance: none; -ms-appearance: none; -o-appearance: none; appearance: none; font-size: 11px; line-height: 20px; background: none; border: none; color: #bbb; outline: none; margin-right: 10px">
+            UI Mode: <select onchange="document.location = window.location.pathname + '?mode=' + this.value " style="-webkit-appearance: none; -moz-appearance: none; -ms-appearance: none; -o-appearance: none; appearance: none; font-size: 11px; font-weight: 500; line-height: 20px; background: none; border: none; color: #fff; outline: none; margin: 0; margin-left: 5px;">
               #{options}
             </select>
           HTML
