f8084e741123331cb3279030d5e1b4903756a48e
Bryan Powell <bryan@metabahn.com>
2019-04-08 14:19:29 -0700

    Move app helpers into their own module, require app instance for globals

---
+++ pakyow-core/lib/pakyow/helpers/app.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "forwardable"
+
+module Pakyow
+  module Helpers
+    # Convenience methods for interacting with the app object.
+    #
+    module App
+      extend Forwardable
+
+      attr_reader :app
+
+      # @!method config
+      #   Delegates to {app}.
+      #
+      #   @see App#config
+      def_delegators :app, :config
+
+      # @!method path
+      #   @return builds the path to a named route (see {Paths#path})
+      #
+      # @!method path_to
+      #   @return builds the path to a route, following a trail of names (see {Paths#path_to})
+      def_delegators :"app.endpoints", :path, :path_to
+    end
+  end
+end
