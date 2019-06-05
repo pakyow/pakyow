f5e72d96e104fc62b9bff4d625ccc061550199e5
Bryan Powell <bryan@metabahn.com>
2019-06-05 16:16:20 -0700

    Set the originating source on updated results

--- pakyow-data/lib/pakyow/data/sources/relational/command.rb
+++ pakyow-data/lib/pakyow/data/sources/relational/command.rb
@@ -366,7 +366,9 @@ module Pakyow
                   )
 
                   updated_source.instance_variable_set(:@results, original_dataset.map { |original_object|
-                    original_object.class.new(original_object.values.merge(final_values))
+                    new_object = original_object.class.new(original_object.values.merge(final_values))
+                    new_object.originating_source = original_object.originating_source
+                    new_object
                   })
 
                   updated_source.instance_variable_set(:@original_results, original_dataset)
