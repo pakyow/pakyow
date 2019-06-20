1ff250ca0b33e37709f069bc19e35cbf7f851215
Bryan Powell <bryan@metabahn.com>
2019-06-20 13:22:05 -0700

    Fix bug when a source in a container has already been finalized

---
+++ pakyow-data/spec/integration/container_finalization_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe "finalizing a container" do
+  let :container do
+    Pakyow::Data::Container.new(
+      connection: connection,
+      sources: [source],
+      objects: []
+    )
+  end
+
+  let :connection do
+    Class.new do
+      module Commands
+      end
+
+      module DatasetMethods
+      end
+
+      def self.adapter
+        nil
+      end
+
+      def self.types
+        {}
+      end
+    end
+  end
+
+  let :source do
+    Class.new(Pakyow::Data::Sources::Relational) do
+      attribute :foo
+    end
+  end
+
+  context "container has already been finalized" do
+    before do
+      container.finalize!([])
+    end
+
+    it "does not fail" do
+      expect {
+        container.finalize!([])
+      }.not_to raise_error
+    end
+  end
+end
