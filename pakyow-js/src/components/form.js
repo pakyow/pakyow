5e41f0fa6b0545f61decfff9f80d75f9f9649a6d
Bryan Powell <bryan@metabahn.com>
2019-06-20 20:51:07 -0700

    Refactor form to use component state transitions

--- pakyow-js/src/components/form.js
+++ pakyow-js/src/components/form.js
@@ -1,5 +1,39 @@
 pw.define("form", {
   appear() {
+    let $fields = this.view.query("input, textarea, button, select");
+    let $focused;
+
+    // Set the element currently in focus to be refocused later.
+    //
+    this.enter("submitting", () => {
+      $focused = document.querySelector(":focus");
+    });
+
+    // Set all fields as disabled.
+    //
+    this.enter("submitting", () => {
+      $fields.forEach((view) => {
+        view.node.disabled = true;
+      });
+    });
+
+    // Reenable all fields.
+    //
+    this.leave("submitting", () => {
+      $fields.forEach((view) => {
+        view.node.disabled = false;
+      });
+    });
+
+    // Refocus on the element in focus ahead of submitting.
+    //
+    this.leave("submitting", () => {
+      if ($focused) {
+        $focused.focus();
+        $focused = null;
+      }
+    });
+
     this.node.addEventListener("submit", (event) => {
       if (pw.ui.modifierKeyPressed) {
         return;
@@ -7,30 +41,26 @@ pw.define("form", {
 
       if (pw.server.reachable) {
         event.preventDefault();
-        pw.send(event.target.action, {
-          method: event.target.method,
-          data: new FormData(event.target),
-          success: (xhr) => {
-            event.target.reset();
-            pw.ui.visit(xhr.responseURL, xhr);
-          },
-          complete: (xhr) => {
-            $fields.forEach((view) => {
-              view.node.disabled = false;
-            });
 
-            if ($focused) {
-              $focused.focus();
-              $focused = null;
+        // Submit the form in the background.
+        //
+        pw.send(this.node.action, {
+          method: this.node.method,
+          data: new FormData(this.node),
+          success: (xhr) => {
+            this.node.reset();
+            this.transition("succeeded", xhr);
+
+            if (typeof this.config.handle_success === "undefined" || this.config.handle_success === "true") {
+              pw.ui.visit(xhr.responseURL, xhr);
             }
+          },
+          error: (xhr) => {
+            this.transition("failed", xhr);
           }
         });
 
-        let $focused = document.querySelector(":focus");
-        let $fields = new pw.View(event.target).query("input, textarea, button, select");
-        $fields.forEach((view) => {
-          view.node.disabled = true;
-        });
+        this.transition("submitted");
       } else {
         // submit normally
       }
