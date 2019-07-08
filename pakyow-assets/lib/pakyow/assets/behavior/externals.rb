fb6a1f018768b5012ef4d2ceb1be41974f7f91ea
Bryan Powell <bryan@metabahn.com>
2019-07-08 13:06:24 -0700

    Pakyow.js 1.0.0-rc.1

--- pakyow-assets/lib/pakyow/assets/behavior/externals.rb
+++ pakyow-assets/lib/pakyow/assets/behavior/externals.rb
@@ -25,7 +25,7 @@ module Pakyow
         apply_extension do
           after "boot", "fetch.assets.externals" do
             if config.assets.externals.pakyow
-              external_script :pakyow, "^1.0.0-alpha.17", package: "@pakyow/js", files: [
+              external_script :pakyow, "^1.0.0-rc.1", package: "@pakyow/js", files: [
                 "dist/pakyow.js",
                 "dist/components/confirmable.js",
                 "dist/components/form.js",
--- pakyow-js/src/version.js
+++ pakyow-js/src/version.js
@@ -1,1 +1,1 @@
-export default "1.0.0-alpha.17";
+export default "1.0.0-rc.1";
