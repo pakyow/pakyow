b2686888754d715323df87a1049e019e50253de9
Bryan Powell <bryan@metabahn.com>
2019-07-25 12:09:58 -0700

    Update external assets when creating a project

--- pakyow-core/lib/pakyow/generators/project.rb
+++ pakyow-core/lib/pakyow/generators/project.rb
@@ -16,6 +16,12 @@ module Pakyow
         end
       end
 
+      after "generate" do
+        Bundler.with_clean_env do
+          run "bundle exec pakyow assets:update", message: "Updating external assets"
+        end
+      end
+
       def generating_locally?
         local_pakyow = Gem::Specification.sort_by { |g| [g.name.downcase, g.version] }.group_by(&:name).detect { |k, _| k == "pakyow" }
         !local_pakyow || local_pakyow.last.last.version < Gem::Version.new(Pakyow::VERSION)
