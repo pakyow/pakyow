df8a119a4b91feaf4c1b38c73f36083d7704fe91
Bryan Powell <bryan@metabahn.com>
2019-11-04 13:11:29 -0800

    Fix assets specs that rely on the latest pakyow.js release

--- pakyow-assets/spec/spec_helper.rb
+++ pakyow-assets/spec/spec_helper.rb
@@ -12,11 +12,38 @@ require "pakyow/assets"
 
 require_relative "../../spec/helpers/mock_handler"
 
+module GlobalContext
+  extend RSpec::SharedContext
+
+  let :latest_pakyow_js do
+    @latest_pakyow_js
+  end
+end
+
 RSpec.configure do |spec_config|
+  spec_config.before :all do
+    @latest_pakyow_js = `npm show @pakyow/js version`.strip
+  end
+
   spec_config.before do
+    local = self
     @default_app_def = Proc.new do
       configure do
         config.root = File.expand_path("../support/app", __FILE__)
+      end
+
+      define_method :pakyow_js_version do
+        local.latest_pakyow_js
+      end
+
+      after "initialize" do
+        self.class.__plugs.each do |plug|
+          plug.class_exec do
+            define_method :pakyow_js_version do
+              local.latest_pakyow_js
+            end
+          end
+        end
       end
     end
   end
@@ -24,6 +51,9 @@ RSpec.configure do |spec_config|
   spec_config.after do
     Pakyow::Assets::Babel.instance_variable_set(:@context, nil)
   end
+
+  spec_config.include GlobalContext
 end
 
 require_relative "../../spec/context/app_context"
+require_relative "../../spec/context/suppressed_output_context"
