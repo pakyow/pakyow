26c9ba2cf0d8565d5756ce9f733451ed10dc0279
Bryan Powell <bryan@metabahn.com>
2019-12-14 15:24:48 -0800

    Use async-http instead of http for downloading externals
    The http gem isn't compatible with Ruby 2.7.

--- pakyow-assets/lib/pakyow/assets/external.rb
+++ pakyow-assets/lib/pakyow/assets/external.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "http"
-
 require "pakyow/support/cli/runner"
 
 module Pakyow
@@ -58,45 +56,81 @@ module Pakyow
         end
 
         Support::CLI::Runner.new(message: "Fetching #{file_with_version}").run do |runner|
-          begin
-            response = HTTP.follow(true).get(
+          Async do
+            downloader = Downloader.new(
+              File.join(@config.externals.provider, package_with_version, file.to_s)
+            ).perform
+
+            FileUtils.mkdir_p(@config.externals.path)
+
+            fetched_version = downloader.path.to_s.split(@package.to_s, 2)[1].split("/", 2)[0].split("@", 2)[1]
+            file_basename = File.basename(file.to_s, File.extname(file.to_s))
+
+            local_path = if file && file_basename != name.to_s
               File.join(
-                @config.externals.provider,
-                package_with_version,
-                file.to_s
+                @config.externals.path,
+                "#{name}@#{fetched_version}__#{file_basename}.js"
               )
-            )
-
-            if response.code == 200
-              FileUtils.mkdir_p(@config.externals.path)
-
-              fetched_version = response.uri.to_s.split(@package.to_s, 2)[1].split("/", 2)[0].split("@", 2)[1]
-              file_basename = File.basename(file.to_s, File.extname(file.to_s))
-
-              local_path = if file && file_basename != name.to_s
-                File.join(
-                  @config.externals.path,
-                  "#{name}@#{fetched_version}__#{file_basename}.js"
-                )
-              else
-                File.join(
-                  @config.externals.path,
-                  "#{name}@#{fetched_version}.js"
-                )
-              end
-
-              File.open(local_path, "w") do |fp|
-                fp.write(response.body.to_s)
-              end
-
-              runner.succeeded
             else
-              runner.failed(response.to_s)
+              File.join(
+                @config.externals.path,
+                "#{name}@#{fetched_version}.js"
+              )
             end
-          rescue HTTP::ConnectionError => error
+
+            File.open(local_path, "w") do |fp|
+              fp.write(downloader.body.to_s)
+            end
+
+            runner.succeeded
+          rescue Downloader::Failed => error
             runner.failed(error.to_s)
           end
         end
+      end
+
+      require "uri"
+
+      require "async"
+      require "async/http/internet"
+
+      # @api private
+      class Downloader
+        attr_reader :status, :body, :path
+
+        def initialize(uri)
+          @uri = URI.parse(uri)
+        end
+
+        def perform
+          get(@uri.path); self
+        end
+
+        private def get(path)
+          @path = path
+          internet = Async::HTTP::Internet.new
+          response = internet.get(build_uri(path))
+          @status = response.status
+
+          if response.status == 301 || response.status == 302
+            get(response.headers["location"])
+          elsif response.status >= 500
+            raise Failed, "Unexpected response status: #{response.status}"
+          else
+            @body = response.body.read
+          end
+        rescue SocketError => error
+          raise Failed.build(error)
+        ensure
+          response&.close
+          internet&.close
+        end
+
+        private def build_uri(path)
+          File.join("#{@uri.scheme}://#{@uri.host}", path)
+        end
+
+        class Failed < Error; end
       end
     end
   end
