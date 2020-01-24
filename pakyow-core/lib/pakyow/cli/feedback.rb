4d29ded0f8dc4af067b0acd9db9ad98bdb017c16
Bryan Powell <bryan@metabahn.com>
2020-01-24 13:15:56 -0800

    Build help through cli feedback

--- pakyow-core/lib/pakyow/cli/feedback.rb
+++ pakyow-core/lib/pakyow/cli/feedback.rb
@@ -45,7 +45,71 @@ module Pakyow
       end
 
       def usage(command, describe: true)
-        @output.puts command.help(describe: describe)
+        required_arguments = command.send(:sorted_arguments).select { |_, argument|
+          argument[:required]
+        }.map { |key, _|
+          "[#{key.to_s.upcase}]"
+        }.join(" ")
+
+        required_options = command.send(:sorted_options).select { |_, option|
+          option[:required]
+        }.map { |key, _|
+          "--#{key}=#{key}"
+        }.join(" ")
+
+        # text = String.new
+
+        if describe
+          @output.puts Support::CLI.style.blue.bold(command.description)
+        end
+
+        @output.puts <<~HELP
+
+          #{Support::CLI.style.bold("USAGE")}
+            $ pakyow #{[command.name, required_arguments, required_options].reject(&:empty?).join(" ")}
+        HELP
+
+        if command.arguments.any?
+          @output.puts <<~HELP
+
+            #{Support::CLI.style.bold("ARGUMENTS")}
+          HELP
+
+          longest_length = command.arguments.keys.map(&:to_s).max_by(&:length).length
+          command.send(:sorted_arguments).each do |key, argument|
+            description = Support::CLI.style.yellow(argument[:description])
+            description += Support::CLI.style.red(" (required)") if argument[:required]
+            @output.puts "  #{key.upcase}".ljust(longest_length + 4) + description
+          end
+        end
+
+        if command.options.any?
+          @output.puts <<~HELP
+
+            #{Support::CLI.style.bold("OPTIONS")}
+          HELP
+
+          longest_length = (command.options.keys + command.flags.keys).map(&:to_s).max_by(&:length).length
+          command.send(:sorted_options_and_flags).each do |key, option|
+            description = Support::CLI.style.yellow(option[:description])
+
+            if option[:required]
+              description += Support::CLI.style.red(" (required)")
+            end
+
+            prefix = if command.flags.key?(key)
+              "      --#{key}"
+            else
+              if command.short_names.key?(key)
+                "  -#{key.to_s[0]}, --#{key}=#{key}"
+              else
+                "      --#{key}=#{key}"
+              end
+            end
+
+            @output.puts prefix.ljust(longest_length * 2 + 11) + description
+          end
+        end
       end
     end
   end
