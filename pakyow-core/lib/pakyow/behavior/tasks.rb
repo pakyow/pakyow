5b1ecae6149a55cc9ce2b5ba2e0b4d1faff488f5
Bryan Powell <bryan@metabahn.com>
2020-01-24 07:14:11 -0800

    Move load_tasks into the tasks behavior

--- pakyow-core/lib/pakyow/behavior/tasks.rb
+++ pakyow-core/lib/pakyow/behavior/tasks.rb
@@ -9,6 +9,18 @@ module Pakyow
 
       apply_extension do
         class_state :tasks, default: []
+
+        # @api private
+        def load_tasks
+          require "pakyow/task"
+          tasks.clear
+
+          config.tasks.paths.uniq.each_with_object(tasks) do |tasks_path, tasks|
+            Dir.glob(File.join(File.expand_path(tasks_path), "**/*.rake")).each do |task_path|
+              tasks.concat(Pakyow::Task::Loader.new(task_path).__tasks)
+            end
+          end
+        end
       end
     end
   end
