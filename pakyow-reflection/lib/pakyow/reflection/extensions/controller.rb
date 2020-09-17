4052ca2f8d5e3d8ff1f9774eefc284226303ce4e
Bryan Powell <bryan@metabahn.com>
2020-09-17 11:16:23 -0700

    Resolve two more warnings in 2.5.8

--- pakyow-reflection/lib/pakyow/reflection/extensions/controller.rb
+++ pakyow-reflection/lib/pakyow/reflection/extensions/controller.rb
@@ -181,9 +181,9 @@ module Pakyow
           with_reflected_action do |reflected_action|
             if connection.form && origin = connection.form[:origin]
               if instance_variable_defined?(:@object)
-                if route = self.class.routes[:get].find { |route| route.name == :show }
+                if route = self.class.routes[:get].find { |each_route| each_route.name == :show }
                   route.build_path(self.class.path_to_self, **params.merge(@object.one.to_h))
-                elsif route = self.class.routes[:get].find { |route| route.name == :list }
+                elsif route = self.class.routes[:get].find { |each_route| each_route.name == :list }
                   route.build_path(self.class.path_to_self, **params)
                 else
                   origin
